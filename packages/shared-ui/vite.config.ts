79cfd17674eff4c1f47912a8800892e063c39415
vibrantCeo <bmejiadeveloper2@gmail.com>
2022-12-10 13:35:44 -0800

    vite-config react jsx-runtime

--- packages/shared-ui/vite.config.ts
+++ packages/shared-ui/vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
 import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    // react(),
+    react({jsxRuntime: "automatic"}),
     dts({
       insertTypesEntry: true,
       outputDir: "dist/types"
