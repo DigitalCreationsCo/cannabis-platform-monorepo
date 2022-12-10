6ebb975f8111894e350a1a6802abf176d07fc486
vibrantCeo <bmejiadeveloper2@gmail.com>
2022-12-10 02:27:00 -0800

    add next eslint rules

--- packages/shared-config/.eslintrc.js
+++ packages/shared-config/.eslintrc.js
@@ -1,10 +1,22 @@
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
-    plugins: ['prettier', 'react', 'react-hooks'],
+    extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier', 'next', 'next/core-web-vitals'],
+    plugins: ['prettier', 'react', 'react-hooks', 'simple-import-sort', ],
     rules: {
-        eqeqeq: 'error',
+        // eqeqeq: 'error',
+        // semi: 'error',
         'no-console': 'warn',
-        'prettier/prettier': 'error',
+        'prettier/prettier': ['error'],
+        '@next/next/no-html-link-for-pages': 'off',
+        'simple-import-sort/imports': 'error',
+        'simple-import-sort/exports': 'error',
+        'no-unused-vars': 'error',
+        'prefer-const': 'error',
+        'no-irregular-whitespace': 'error',
+        'no-trailing-spaces': 'error',
+        'no-empty-function': 'error',
+        'no-duplicate-imports': 'error',
+        'newline-after-var': 'error',
+        'camelcase': 'error',
     },
     parserOptions: {
         sourceType: 'module',
@@ -18,6 +30,11 @@ module.exports = {
         node: true,
         es6: true,
         jest: true,
+    },
+    settings: {
+        next: {
+            rootDir: ['apps/*/', 'packages/*/'],
+        },
     },
     ignorePatterns: ['node_modules', 'build', 'dist', 'public'],
 };
