9e775e5f1b6e42409ffabbac0c90607cd7adc4af
vibrantCeo <bmejiadeveloper2@gmail.com>
2022-12-10 23:07:56 -0800

    fix merge conflicts

--- packages/shared-ui/vite.config.ts
+++ packages/shared-ui/vite.config.ts
@@ -1,31 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dts from 'vite-plugin-dts'
-import path from 'node:path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import path from 'node:path';
 
 export default defineConfig({
-  plugins: [
-    react({jsxRuntime: "classic"}),
-    dts({
-      insertTypesEntry: true,
-      outputDir: "dist/types"
-    }),
-  ],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'shared-ui',
-      formats: [ 'es', 'umd' ],
-      fileName: (format) => `shared-ui.${format}.js`,
+    plugins: [
+        react({ jsxRuntime: 'classic' }),
+        dts({
+            insertTypesEntry: true,
+            outputDir: 'dist/types',
+        }),
+    ],
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, 'src/index.ts'),
+            name: 'shared-ui',
+            formats: ['es', 'umd'],
+            fileName: (format) => `shared-ui.${format}.js`,
+        },
+        rollupOptions: {
+            external: ['react', 'react-dom'],
+            output: {
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM',
+                },
+            },
+        },
     },
-    rollupOptions: {
-      external: [ 'react', 'react-dom' ],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-              },
-      },
-    },
-  },
 });
