cdb2670b631a74de35cd723e5630d443abefa97c
vibrantCeo <bmejiadeveloper2@gmail.com>
2023-01-07 01:09:54 +0900

    delete old server src files

--- server/main/srcOLD/api/data-access/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as DriverDA } from './DriverDA';
-export { default as ShopDA } from './ShopDA';
-export { default as UserDA } from './UserDA';
-export { default as OrganizationDA } from './OrganizationDA';
-export { default as OrderDA } from './OrderDA';
--- server/main/srcOLD/api/routes/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as driver } from './driver.route';
-export { default as shop } from './shop.route';
-export { default as organization } from './organization.route';
-export { default as user } from './user.route';
