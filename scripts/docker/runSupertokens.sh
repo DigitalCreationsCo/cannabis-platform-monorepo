623a8e2a682f77c27616574433fe53d9e232c107
vibrantCeo <bmejiadeveloper2@gmail.com>
2023-01-07 19:56:52 +0900

    add supertokens docker script

---
+++ scripts/docker/runSupertokens.sh
@@ -0,0 +1,4 @@
+docker run \
+  -p 3309:3309 \
+  -e MYSQL_CONNECTION_URI="mysql://5rym7hkijdnzxdirhiw8:pscale_pw_k7kY1HvI5XnaRKePgW52jv01EQXj8FxJFAhL43YCTzP@us-east.connect.psdb.cloud/cannabis_delivery_v1?ssl={"rejectUnauthorized":true}" \
+  -d registry.supertokens.io/supertokens/supertokens-mysql
