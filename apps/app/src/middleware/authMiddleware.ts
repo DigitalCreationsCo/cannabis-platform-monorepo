b60dccaa62fd99ebec9f671c2107a22d074d4cf9
vibrantCeo <bmejiadeveloper2@gmail.com>
2023-01-10 17:50:58 +0900

    added VerifySession func to authm iddleware

--- apps/app/src/middleware/authMiddleware.ts
+++ apps/app/src/middleware/authMiddleware.ts
@@ -1,6 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import {User} from '@cd/data-access'
 import { SessionInformation } from 'supertokens-node/recipe/session';
+import { verifySession } from "supertokens-node/recipe/session/framework/express";
+import { SessionRequest } from "supertokens-node/framework/express";
+import EmailPassword from "supertokens-node/recipe/emailpassword";
+
 // import { getToken } from 'next-auth/jwt';
 
 export type ExtendRequest = NextApiRequest & {
@@ -9,9 +13,12 @@ export type ExtendRequest = NextApiRequest & {
   organizationId?: string;
 };
 
-export default async function authMiddleware(req: ExtendRequest, res: NextApiResponse, next: Function) {
+export default async function authMiddleware(req: SessionRequest, res: any, next: Function) {
   try {
-    // const token = await getToken({ req });
+    await verifySession()(req, res, next())
+    let userId = req?.session?.getUserId();
+    console.log('auth middleware user id: ', userId)
+    // let userInfo = await EmailPassword.getUserById({userId})
     next();
   } catch (error) {
     throw new Error(error.message);
