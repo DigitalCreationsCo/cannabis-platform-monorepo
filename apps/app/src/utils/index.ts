8ece9afa426d5083d4693a8ffe7cfed055bb678e
vibrantCeo <bmejiadeveloper2@gmail.com>
2023-01-15 07:25:48 -0500

    resolved type errors OK 01-15-2023

--- apps/app/src/utils/index.ts
+++ apps/app/src/utils/index.ts
@@ -1,4 +1,3 @@
-export { default as urlBuilder } from './urlBuilder';
 export { default as debounce } from './debounce';
-export * from './fetchData';
 export * from './sale';
+export { default as urlBuilder } from './urlBuilder';
