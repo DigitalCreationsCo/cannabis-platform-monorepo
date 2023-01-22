a2e59222147d7c44088753e4acb389877676fda8
vibrantCeo <bmejiadeveloper2@gmail.com>
2023-01-22 03:35:49 -0500

    fix server main env load

--- server/main/index.ts
+++ server/main/index.ts
@@ -1,19 +1,19 @@
-const nodeEnv = process.env.NODE_ENV;
-console.info(' ♞ Server Main running in ' + nodeEnv + ' mode.');
-
 import prisma from '@cd/data-access';
-import { loadEnv } from '@cd/shared-config/config/loadEnv';
+import { loadEnv } from '@cd/shared-config/config/loadEnv.js';
 import { config } from 'dotenv';
 import { expand } from 'dotenv-expand';
-import { connectDb, server } from './src';
-expand(config({ path: loadEnv(nodeEnv) }));
 
+const nodeEnv = process.env.NODE_ENV;
+expand(config({ path: loadEnv(nodeEnv) }));
 const port = process.env.SERVER_MAIN_PORT || 'xxxx';
+
+import { connectDb, server } from './src';
 
 connectDb(prisma)
     .then(() => {
         server.listen(port, () => {
             console.log(` 🚀 Server Main listening on port ${port}.`);
+            console.info(' ♞ Server Main running in ' + nodeEnv + ' mode.');
         });
     })
     .catch((err) => {
