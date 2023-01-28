1335bd0ab2095f5e0d9561a7de6e43a2efa6ced5
vibrantCeo <bmejiadeveloper2@gmail.com>
2023-01-28 18:39:15 -0500

    wip: try shared-ui build from docker container

--- packages/shared-ui/vite.config.ts
+++ packages/shared-ui/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import dts from 'vite-plugin-dts';
 import path from 'node:path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
     server: {
