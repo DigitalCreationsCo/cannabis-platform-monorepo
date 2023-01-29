5c9604b8eb16d5ce346fdffc21f8f42b6e3abcbd
vibrantCeo <bmejiadeveloper2@gmail.com>
2023-01-29 03:47:01 -0500

    shared-ui build is failing, troubleshoot

--- packages/shared-ui/vite.config.ts
+++ packages/shared-ui/vite.config.ts
@@ -4,15 +4,14 @@ import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-    server: {
-        watch: {
-            ignored: ['!**/node_modules/@cd/shared-config/**'],
-        },
-    },
+    // server: {
+    //     watch: {
+    //         ignored: ['!**/node_modules/@cd/shared-config/**'],
+    //     },
+    // },
     plugins: [
         react({ jsxRuntime: 'classic' }),
         dts({
-            insertTypesEntry: true,
             outputDir: 'dist',
         }),
     ],
@@ -20,25 +19,16 @@ export default defineConfig({
         lib: {
             entry: path.resolve(__dirname, 'src/index.ts'),
             name: '@cd/shared-ui',
-            formats: ['es', 'cjs'],
-            fileName: (format) => `shared-ui.${format}.js`,
+            fileName: 'shared-ui',
         },
         rollupOptions: {
-            plugins: [
-                // incremental(),
-                // incremental.fixSNE(),
-            ],
             external: ['react', 'react-dom'],
             output: {
                 globals: {
                     react: 'React',
                     'react-dom': 'ReactDOM',
                 },
-                // preserveModules: true,
             },
         },
-    },
-    optimizeDeps: {
-        exclude: ['@cd/shared-config'],
     },
 });
