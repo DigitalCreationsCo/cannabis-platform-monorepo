8637fbbd4911bd8cb1a27aff3b0cef60452e098e
vibrantCeo <bmejiadeveloper2@gmail.com>
2023-01-30 22:19:19 -0500

    turborepo reconfig, add watch, and dev, dev:init commands

--- packages/shared-ui/vite.config.ts
+++ packages/shared-ui/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import dts from 'vite-plugin-dts';
 import path from 'node:path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
     server: {
@@ -24,17 +24,13 @@ export default defineConfig({
             fileName: (format) => `shared-ui.${format}.js`,
         },
         rollupOptions: {
-            plugins: [
-                // incremental(),
-                // incremental.fixSNE(),
-            ],
+            plugins: [],
             external: ['react', 'react-dom'],
             output: {
                 globals: {
                     react: 'React',
                     'react-dom': 'ReactDOM',
                 },
-                // preserveModules: true,
             },
         },
     },
