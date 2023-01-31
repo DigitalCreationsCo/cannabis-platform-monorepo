f43cc588ad5fae1def57e9c7b180fd936224ee82
vibrantCeo <bmejiadeveloper2@gmail.com>
2023-01-31 11:59:11 -0500

    shared-ui rollup ts and css build ok

--- packages/shared-ui/rollup.config.js
+++ packages/shared-ui/rollup.config.js
@@ -1,20 +1,30 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
 import postcss from 'rollup-plugin-postcss';
 import packageJson from './package.json' assert { type: 'json' };
-export default {
-    input: 'src/index.ts',
-    output: [
-        {
-            file: packageJson.main,
-            format: 'cjs',
-            name: '@cd/shared-ui',
-        },
-        {
-            file: packageJson.module,
-            format: 'es',
-        },
-    ],
-    plugins: [resolve(), commonjs(), postcss(), typescript({ tsconfig: './tsconfig.json' })],
-};
+
+export default [
+    {
+        input: 'src/index.ts',
+        output: [
+            {
+                file: packageJson.main,
+                format: 'cjs',
+                name: '@cd/shared-ui',
+            },
+            {
+                file: packageJson.module,
+                format: 'es',
+            },
+        ],
+        plugins: [resolve(), commonjs(), postcss(), typescript({ tsconfig: './tsconfig.json' })],
+    },
+    {
+        input: 'dist/shared-ui.es.js',
+        output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+        external: [/\.css$/],
+        plugins: [dts()],
+    },
+];
