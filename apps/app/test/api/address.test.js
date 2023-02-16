06e28a9058ef7560241935265eb2e30d7181d041
vibrantCeo <bmejiadeveloper2@gmail.com>
2023-02-16 12:48:58 -0500

    add address test cases

--- apps/app/test/api/address.test.js
+++ apps/app/test/api/address.test.js
@@ -11,8 +11,23 @@
 // // issue here, is getting the correct organizationId from the session object, and getting it in axios call.
 // // maybe mock the session
 // // another issue is the caching that happens on the handler side, can interfere with test
+
+
+// describe('POST /api/users/2/address', () => {
+//     test('add address return the correct server call with params', async () => {
+//         const { req, res } = createMocks({
+//             method: 'GET',
+//             query: {
+//                 animal: 'dog',
+//             },
+//         });
+//         await ordersHandler(req, res);
+//         expect(axios).toHaveBeenLastCalledWith(urlBuilder.main.ordersByOrgId('3'));
+//     });
+// });
+
 // describe('DELETE /api/users/2/address/3', () => {
-//     test('getUsersByOrgId return the correct server call with params', async () => {
+//     test('delete address return the correct server call with params', async () => {
 //         const { req, res } = createMocks({
 //             method: 'GET',
 //             query: {
