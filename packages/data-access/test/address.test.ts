956db03544c25e5f893473111aa1a64b62e71dc2
vibrantCeo <bmejiadeveloper2@gmail.com>
2023-02-16 12:48:58 -0500

    add address test cases

--- packages/data-access/test/address.test.ts
+++ packages/data-access/test/address.test.ts
@@ -1,5 +1,14 @@
 import { deleteAddressByIdAndUser, findAddressByIdAndUser } from '../src/address.js';
 
+test('add address with userId', async () => {
+    const address = {
+       ...
+    };
+
+    // prismaMock.organization.findUnique.mockResolvedValue(address)
+    await expect(findAddressByIdAndUser({addressId: '2', id: '1'})).resolves.toEqual(address);
+});
+
 test('find address with the given addressId and userId', async () => {
     const address = {
        ...
