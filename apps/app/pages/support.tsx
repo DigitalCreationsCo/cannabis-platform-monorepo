0ea9a72aa29ee02c37e8f2a25c52327118102070
vibrantCeo <bmejiadeveloper2@gmail.com>
2023-02-22 20:48:51 -0500

    WIP: production envrionement, with Docker

--- apps/app/pages/support.tsx
+++ apps/app/pages/support.tsx
@@ -1,21 +1,20 @@
-import { Grid, H4, H5, Icons, Page } from "@cd/shared-ui";
-import { PageHeader, ProtectedComponent } from "../src/components";
+import { Grid, H4, H5, Icons, Page } from '@cd/shared-ui';
+import { PageHeader, ProtectedComponent } from '../src/components';
 
 function Support() {
     return (
-        <ProtectedComponent><Page className="md:max-w-1/2">
-            <PageHeader
-                title='Support' Icon={ Icons.Shield }
-            />
-            <Grid className="space-y-4 px-2">
-                <H5 className='whitespace-pre-line'>{ ` 
+        <ProtectedComponent>
+            <Page className="md:max-w-1/2">
+                <PageHeader title="Support" Icon={Icons.Shield} />
+                <Grid className="space-y-4 px-2">
+                    <H5 className="whitespace-pre-line">{` 
             We're working around the clock to deliver a world class support service for your business.
             For software support, please dial the toll-free support phone number. `}</H5>
-                <H4 className='text-left whitespace-pre-line'>{ 
-                `24 / 7 Gras Support Phone Number: 
+                    <H4 className="text-left whitespace-pre-line">{`24 / 7 Gras Support Phone Number: 
                 1-800-GREEN-35`}</H4>
                 </Grid>
-        </Page></ProtectedComponent>
+            </Page>
+        </ProtectedComponent>
     );
 }
 
---
+++ apps/app/pages/welcome.tsx
@@ -0,0 +1,14 @@
+import { ProtectedComponent } from '../src/components';
+import LandingPage from '../src/components/Landing';
+
+function WelcomePage() {
+    return (
+        <ProtectedComponent>
+            <Page>
+                <LandingPage />
+            </Page>
+        </ProtectedComponent>
+    );
+}
+
+export default WelcomePage;
