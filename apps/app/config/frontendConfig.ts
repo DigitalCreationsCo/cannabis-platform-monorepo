42750142406f0ed5e88ee5747a7e16fb794e0c43
Bryant Mejia <vibrantceo@Bryants-MacBook-Pro.local>
2023-03-02 15:24:28 -0500

    user/create-account, signup flow works OK

--- apps/app/config/frontendConfig.ts
+++ apps/app/config/frontendConfig.ts
@@ -1,5 +1,4 @@
 import { appInfo } from '@cd/shared-config/auth/appInfo';
-import { RecipeEventWithSessionContext } from 'supertokens-auth-react/lib/build/recipe/session/types';
 import EmailPassword from 'supertokens-auth-react/recipe/emailpassword';
 import Session from 'supertokens-auth-react/recipe/session';
 
@@ -17,35 +16,11 @@ export const frontendConfig = () => {
                                 console.log('frontend signup input: ', input);
                                 const response = await originalImplementation.signUp(input);
                                 console.log('frontend signup response: ', response);
-                                if (response.status === 'OK') {
-                                    console.log('frontend signup: session created');
-                                    return { ...response, user: { ...response?.user, ...input.userContext } };
-                                }
+                                if (response.status === 'FIELD_ERROR') return response;
+                                return response;
                             }
                         };
                     }
-                },
-                signInAndUpFeature: {
-                    // signUpForm: {
-                    //     formFields: [
-                    //         { id: 'email', label: 'Email' },
-                    //         { id: 'password', label: 'Password' }
-                    //         { id: 're-password', label: 'Confirm Password' },
-                    //         { id: 'username', label: 'Username' }
-                    //         { id: 'firstName', label: 'First Name' },
-                    //         { id: 'lastName', label: 'Last Name' },
-                    //         { id: 'phone', label: 'Phone' },
-                    //         { id: 'dialCode', label: 'Dial Code' },
-                    //         { id: 'address.countryCode', label: 'Country Code' },
-                    //         { id: 'address.country', label: 'Country' },
-                    //         { id: 'address.city', label: 'City' },
-                    //         { id: 'address.state', label: 'State' },
-                    //         { id: 'address.zipcode', label: 'Zipcode' },
-                    //         { id: 'address.street1', label: 'Street 1' },
-                    //         { id: 'address.street2', label: 'Street 2', optional: true },
-                    //         { id: 'termsAccepted', label: 'Terms Accepted' }
-                    //     ]
-                    // }
                 },
                 onHandleEvent: (context) => {
                     console.log('fe emailpassword context: ', context);
@@ -53,26 +28,27 @@ export const frontendConfig = () => {
                         console.log('fe emailpassword success: ', context);
                         return context;
                     }
+                    return context;
                 }
             }),
             Session.init({
-                onHandleEvent: (event: RecipeEventWithSessionContext) => {
-                    if (event.action === 'UNAUTHORISED' || event.action === 'SIGN_OUT') {
-                        window.location.href = '/welcome';
-                    }
-                    if (event.action === 'SESSION_CREATED') {
-                        console.log('fe session created: ', event.userContext);
-                        if (
-                            event.userContext.memberships?.[0]?.role.toLocaleUpperCase() === 'ADMIN' ||
-                            event.userContext.memberships?.[0]?.role.toLocaleUpperCase() === 'OWNER'
-                        ) {
-                            // window.location.href = '/dashboard';
-                            window.location.href = '/';
-                        } else {
-                            window.location.href = '/';
-                        }
-                    }
-                }
+                // onHandleEvent: (event: RecipeEventWithSessionContext) => {
+                //     if (event.action === 'UNAUTHORISED' || event.action === 'SIGN_OUT') {
+                //         window.location.href = '/welcome';
+                //     }
+                //     if (event.action === 'SESSION_CREATED') {
+                //         console.log('fe session created: ', event.userContext);
+                //         if (
+                //             event.userContext.memberships?.[0]?.role.toLocaleUpperCase() === 'ADMIN' ||
+                //             event.userContext.memberships?.[0]?.role.toLocaleUpperCase() === 'OWNER'
+                //         ) {
+                //             // window.location.href = '/dashboard';
+                //             window.location.href = '/';
+                //         } else {
+                //             window.location.href = '/';
+                //         }
+                //     }
+                // }
             })
         ],
         isInServerLessEnv: false
--- server/main/src/config/backendConfig.ts
+++ server/main/src/config/backendConfig.ts
@@ -1,9 +1,10 @@
-import { findUserWithDetailsByEmail, UserLoginData } from "@cd/data-access";
+import { Address, findUserWithDetailsByEmail, SessionPayload, UserCreateType, UserLoginData } from "@cd/data-access";
 import { appInfo } from "@cd/shared-config/auth/appInfo";
 import Dashboard from "supertokens-node/recipe/dashboard";
 import EmailPassword from "supertokens-node/recipe/emailpassword";
 import STSession from "supertokens-node/recipe/session";
 import { AuthConfig } from "../../interfaces";
+import { UserDA } from "../api/data-access";
 
 export let backendConfig = (): AuthConfig => {
     console.log('SERVER MAIN BACKEND API INFO: ', appInfo)
@@ -25,126 +26,97 @@ export let backendConfig = (): AuthConfig => {
                         { id: 'lastName' },
                         { id: 'phone' },
                         { id: 'dialCode' },
-                        { id: 'address.countryCode' },
-                        { id: 'address.country' },
-                        { id: 'address.city' },
-                        { id: 'address.state' },
-                        { id: 'address.zipcode' },
-                        { id: 'address.street1' },
-                        { id: 'address.street2', optional: true },
+                        { id: 'countryCode' },
+                        { id: 'country' },
+                        { id: 'city' },
+                        { id: 'state' },
+                        { id: 'zipcode' },
+                        { id: 'street1' },
+                        { id: 'street2', optional: true },
                         { id: 'termsAccepted' }
                     ]
                 },
                 override: {
-                    functions(originalImp) {
+                    functions(originalImplementation) {
                         return { 
-                            ...originalImp,
+                            ...originalImplementation,
                             async signIn(input: UserLoginData) {
-                                // try{
-                                if (input) {
-                                    const user = await findUserWithDetailsByEmail(input.email)
-                                    console.log('SERVER MAIN: signin user: ', user)
-                                    // if (userContext === null) {
-                                    //     throw new Error('User does not exist');
-                                    // }
-                                    // if (userContext !== null && userContext.passwordHash === null) {
-                                    //     throw new Error('Please reset your password');
-                                    // }
+                                const user = await findUserWithDetailsByEmail(input.email)
+                                console.log('backend signin user: ', user)
+                                // if (userContext === null) {
+                                //     throw new Error('User does not exist');
+                                // }
+                                // if (userContext !== null && userContext.passwordHash === null) {
+                                //     throw new Error('Please reset your password');
+                                // }
 
-                                    // if (userContext !== null && !isPasswordMatch(input.password, userContext.passwordHash)) {
-                                    //     throw new Error('Invalid password')
-                                    // }
-                                    const response = await originalImp.signIn({...input, userContext: user})
-                                    console.log('response: ', response)
-                                    return response
-                                }
-                            // } catch(error) {
-                            //     console.log(error)
-                            //     return new Error(error)
-                            // }
-                        },
+                                // if (userContext !== null && !isPasswordMatch(input.password, userContext.passwordHash)) {
+                                //     throw new Error('Invalid password')
+                                // }
+                                const response = await originalImplementation.signIn({...input, userContext: user})
+                                console.log('backend signin response: ', response)
+                                return response
+                            },
                             // async signUp(input) {
-                            //     if (input) {
-                            //         console.log('SERVER MAIN: signup input: ', input);
-
-                            //         const userContext = input.userContext
-                            //         // console.log('user   context: ', userContext())
-                            //     const response = await originalImp.signUp(input)
+                            //     const response = await originalImplementation.signUp(input)
+                            //     if (response.status === 'OK') {
+                            //         // set cuid as the user id, for data parity
+                            //         response.user.id = createId()
+                            //     }
+                            //     if (response.status ==="EMAIL_ALREADY_EXISTS_ERROR") return response
                             //     console.log('backend signup response: ', response)
-                                    
-                            //     if (response.status === "OK") {
-                            //         console.log('signup: session created')
-                            //         return response
-
-                            //     }
-                            //         // const user = await UserDA.signup(createUserData);
-                            //         // access token payload
-                            //         // const sessionPayload:SessionPayload = { userId: user.id, username: user.username, email: user.email };
-                                    
-                            //         // const sessionToken = await STSession.createNewSession(res, user.id, sessionPayload, { data: 'SESSION TEST DATA' }, user);
-                            //         // future note: drivers will have only session active on a device.
-                            //         // Drivers will need their own session function for login
-                            //         // const session = await UserDA.createUserSession(sessionToken.getHandle(), sessionPayload, await sessionToken.getExpiry())
-                            //     }
+                            //     return response
                             // }
                         }
                     },
-                    apis(originalImplementation, builder) {
+                    apis(originalImplementation) {
                         return {
                             ...originalImplementation,
                             signUpPOST: async function (input) {
-                                // console.log('! post signup callback input: ', input)
-                                // let response = {...await originalImplementation.signUpPOST(input), user: {...input.formFields}}
-                                // console.log('backend signup response: ', response);
-                                // if (response.status === 'OK') {
-                                //     console.log('backend signup OK');
-                                //     response = { ...response, user: {...response?.user, ...input.userContext } };
-                                //     return response
-                                // }
+                                console.log('backend signUpPost input: ', input)
+                                if (originalImplementation.signUpPOST === undefined) {
+                                    throw Error("backend signUpPost: Something went wrong.");
+                                }
+                                
                                 let response = {...await originalImplementation.signUpPOST(input) }
                                 if (response.status === 'OK') {
                                     const formFieldsArr = Object.values(input.formFields)
-                                    console.log('formFields Array: ', formFieldsArr) // yes
-                                    // console.log('formFields obj: ', Object.values(input.formFields).map((i, index )=> ({'id': i['value']})))
                                     const formFields = formFieldsArr.map((_, index )=> {
                                         const id = formFieldsArr[index]['id']
-                                        return {_,[id]: '1'}
+                                        // console.log('id: ', id)
+                                        // const idVal = _[id]
+                                        // console.log('id value: ', idVal)
+                                        // const idVal2 = _['id']
+                                        // console.log('id value 2: ', idVal2)
+                                        // return {_,[id]: '1'}
+                                        return {[id]: _['value']}
                                     })
+                                    const formFieldsTransformed = Object.assign({}, ...formFields)
+                                    // console.log('form fields transformed: ', formFieldsTransformed)
+                                    response = { ...response, user: {...response.user, ...formFieldsTransformed }} // yes
+                                    console.log('backend signUpPost OK response: ', response)
 
-                                    // const formFields = input.formFields.reduce((acc,cur) => ({...acc, ...cur }), {})
-                                    // const result = formFields.reduce((acc,cur) => ({...acc, 'id': cur['value'] }), {})
-                                    console.log('form fields: ', formFields)
-                                    response = { ...response, user: {...response.user}}
-                                    console.log('backend signup api: ', response)
+                                    const {timeJoined, street1, street2, city, state, zipcode, country, countryCode, ...createUserData} = response.user as EmailPassword.User & UserCreateType & Address
+                                    createUserData.address = {street1, street2, city, state, zipcode, country, countryCode}
+                                    createUserData.createdAt = new Date(timeJoined)
+                                    console.log('create user data: ', createUserData)
+
+                                    const user = await UserDA.createUser(createUserData);
+                                    console.log ('backend signUpPost created user: ', user)
+                                    // const sessionToken = await STSession.createNewSession(res, user.id, sessionPayload, { data: 'SESSION TEST DATA' }, user);
+
+                                    // future note: drivers will have only session active on a device.
+                                    // Drivers will need their own session function for login
+                            
+                                    const sessionPayload:SessionPayload = { userId: user.id, username: user.username, email: user.email };
+                                    const dbSession = await UserDA.createUserSession(response.session.getHandle(), sessionPayload, await response.session.getExpiry())
+                                    console.log ('backend signUpPost created session: ', dbSession)
                                 }
+                                console.log('backend signUpPost response: ', response)
                                 return response
                             },
                         }
                     },
-                    // apis: async (originalImplementation) => {
-                    //     return {
-                    //         ...originalImplementation,
-                    //         // signUpPOST: async function (input) {
-                    //         //     console.log(' ! post signup callback')
-                    //         //     if (originalImplementation.signUpPOST === undefined) {
-                    //         //         throw Error("Should never come here");
-                    //         //     }
-    
-                    //         //     // First we call the original implementation of signUpPOST.
-                    //         //     let response = await originalImplementation.signUpPOST(input);
-    
-                    //         //     // Post sign up response, we check if it was successful
-                    //         //     if (response.status === "OK") {
-                    //         //         let { id, email } = response.user;
-    
-                    //         //         // // These are the input form fields values that the user used while signing up
-                    //         //         let formFields = input.formFields;
-                    //         //         // TODO: post sign up logic
-                    //         //     }
-                    //         //     return response;
-                    //         // }
-                    //     }
-                    // },
                 },
             }),
             STSession.init(),
