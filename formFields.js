41eadaec386dc83aa92f440632db6c642d5297a9
AndresMB <bmejiadeveloper2@gmail.com>
2023-03-06 05:40:31 -0500

    clean files

--- formFields.js
+++
@@ -1,5 +0,0 @@
-const formFields = [
-{ id: 'email', value: 'bmejia22321412221122s5@gmail.com' },{ id: 'email', value: 'bmejia22321412221122s5@gmail.com' },{ id: 'email', value: 'bmejia22321412221122s5@gmail.com' }]
-const result = formFields.reduce((acc,cur) => ({...acc, 'id': cur['value'] }), {})
-const result = formFields.reduce((acc,cur) => ({...acc, result['id']: cur['value'] }), {})
-console.log(result)
--- shared-libCOPY/src/hooks/usePagination.ts
+++
@@ -1,8 +0,0 @@
-const usePagination = (currentPage: number, data: Parameters<typeof usePagination>[1][], pageSize = 10) => {
-    const indexOfLastTodo = currentPage * pageSize;
-    const indexOfFirstTodo = indexOfLastTodo - pageSize;
-    const currentData = data.slice(indexOfFirstTodo, indexOfLastTodo);
-    return currentData;
-};
-
-export default usePagination;
