b2e7d9f44476cb946b36945419cf6fa41f13786b
AndresMB <bmejiadeveloper2@gmail.com>
2023-03-05 23:54:23 -0500

    step1:rebuild shop app

---
+++ shop2/config/frontendConfig.ts
@@ -0,0 +1,72 @@
+import { appInfo } from '@cd/shared-config/auth/appInfo';
+import { RecipeEventWithSessionContext } from 'supertokens-auth-react/lib/build/recipe/session/types';
+import EmailPassword from 'supertokens-auth-react/recipe/emailpassword';
+import Session from 'supertokens-auth-react/recipe/session';
+
+export const frontendConfig = () => {
+    console.log('FRONTEND API INFO: ', appInfo);
+    return {
+        appInfo,
+        recipeList: [
+            EmailPassword.init({
+                override: {
+                    functions: (originalImplementation) => {
+                        return {
+                            ...originalImplementation,
+                            async signUp(input) {
+                                // console.log('frontend signup input: ', input);
+                                const response = await originalImplementation.signUp(input);
+                                // console.log('frontend signup response: ', response);
+                                if (response.status === 'FIELD_ERROR') return response;
+                                return response;
+                            },
+                            async signIn(input) {
+                                console.log('frontend signin input: ', input);
+                                const response = await originalImplementation.signIn(input);
+                                console.log('frontend signin response: ', response);
+                                if (response.status === 'FIELD_ERROR') return response;
+                                return response;
+                            }
+                        };
+                    }
+                }
+                // onHandleEvent: (context) => {
+                //     if (context.action === 'SUCCESS') {
+                //         // console.log('fe emailpassword success: ', context);
+                //         return context;
+                //     }
+                //     return context;
+                // }
+            }),
+            Session.init({
+                // override: {
+                //     functions: (originalImplementation) => {
+                //         return {
+                //             ...originalImplementation,
+                //             signOut(input) {
+                //                 return originalImplementation.signOut(input);
+                //             }
+                //         };
+                //     }
+                // }
+                onHandleEvent: (event: RecipeEventWithSessionContext) => {
+                    if (event.action === 'UNAUTHORISED' || event.action === 'SIGN_OUT') {
+                        window.location.href = '/welcome';
+                    }
+                    if (event.action === 'SESSION_CREATED') {
+                        if (
+                            event.userContext.memberships?.[0]?.role.toLocaleUpperCase() === 'ADMIN' ||
+                            event.userContext.memberships?.[0]?.role.toLocaleUpperCase() === 'OWNER'
+                        ) {
+                            // window.location.href = '/dashboard';
+                            window.location.href = '/';
+                        } else {
+                            window.location.href = '/';
+                        }
+                    }
+                }
+            })
+        ],
+        isInServerLessEnv: false
+    };
+};
