e3838b8d5a1ba7c40ec0997cd212885d90911c78
AndresMB <bmejiadeveloper2@gmail.com>
2023-03-09 04:55:29 -0500

    init python geocode web app, for geocding

--- server/location/src/server.ts
+++ server/location/src/server.ts
@@ -1,3 +1,4 @@
+import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
 import http from 'http';
@@ -8,6 +9,8 @@ console.log('node env: ', process.env.NODE_ENV);
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use('/api/v1/healthcheck', (req, res) => {
     return res.status(200).json('OK');
