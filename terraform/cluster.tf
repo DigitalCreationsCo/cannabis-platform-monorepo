d02552d643e454a5a112a0cc25745f29156b1444
AndresMB <bmejiadeveloper2@gmail.com>
2023-03-16 04:07:30 -0400

    create shop dockerfile, docker compose yml file

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -1,5 +1,19 @@
 resource "local_file" "k8s_config" {
-    content = "Actual data coming soon"
+    content = "${nonsensitive(base64decode(linode_lke_cluster.terraform_k8s.kubeconfig))}"
     filename = "${local.k8s_config_file}"
     file_permission = "0600"
+}
+
+resource "linode_lke_cluster" "terraform_k8s" {
+    k8s_version = "${var.k8s_version}"
+    label="${var.k8s_label}"
+    region="${var.k8s_region}"
+    tags="${var.tags}"
+    dynamic "pool" {
+        for_each = var.pools
+        content { 
+            type = pool.value["type"]
+            count = pool.value["count"]
+        }
+    }
 }
