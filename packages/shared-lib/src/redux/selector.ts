60c0912a16fa0b15d10027525308509c5fc8eff5
AndresMB <bmejiadeveloper2@gmail.com>
2023-03-24 00:50:42 -0400

    redux state is persisting OK :)

--- packages/shared-lib/src/redux/selector.ts
+++ packages/shared-lib/src/redux/selector.ts
@@ -1,10 +1,6 @@
-import { AppState } from ".";
 
 // TO DO: Create a custom useSelector hook that will inject this selector into redux, to get type definitions in the selector without having to import the state type into the selector file
-const Selector = (state:AppState) => Object.freeze({
-
-    user: () => state.user.user,
-  
+const Selector = (state) => Object.freeze({
     socket: () => state.socket,
     incomingOrder: () => state.socket.incomingOrder,
     dispatchOrders: () => state.socket.dispatchOrders,
