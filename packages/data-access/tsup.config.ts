2060f141d7f6e985525b325a49333fac280caab6
AndresMB <bmejiadeveloper2@gmail.com>
2023-03-30 15:24:02 -0400

    test, use tsup build

---
+++ packages/data-access/tsup.config.ts
@@ -0,0 +1,18 @@
+import * as dotenv from 'dotenv';
+import { defineConfig } from 'tsup';
+dotenv.config({ path: `../../.env.${process.env.NODE_ENV}` });
+
+export default defineConfig((options) => {
+    return {
+        entry: ['src/index.ts'],
+        splitting: true,
+        clean: true,
+        dts: true,
+        format: ['esm'],
+        platform: 'node',
+        target: ['node16'],
+        sourcemap: !options.watch,
+        // Do not minify node only packages to let patching possible by the consumer (ie: patch-package)
+        minify: false
+    };
+});
