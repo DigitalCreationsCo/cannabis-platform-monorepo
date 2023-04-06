969a7b108a658154f34900f396e6ed6b28506c23
AndresMB <bmejiadeveloper2@gmail.com>
2023-04-05 22:19:49 -0400

    creating injectable delivery-widget

--- packages/delivery-widget/src/views/pages/delivery-button.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-try {
-    const view = fs.readFileSync(path.join(__dirname, './delivery-button.ejs'), 'utf8');
-    // ejs.renderFile(view,{})
-} catch (error) {
-console.log(error)
-}
