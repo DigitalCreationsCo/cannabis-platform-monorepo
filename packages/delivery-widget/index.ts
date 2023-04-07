7724773b6708ef9b94907f9d8d528923c7b7d10a
AndresMB <bmejiadeveloper2@gmail.com>
2023-04-07 15:26:40 -0400

    using browsersync for dev server, serve:hostpage, and serve:js

--- packages/delivery-widget/index.ts
+++
@@ -1,8 +0,0 @@
-var init = function () {
-    console.log('this is the gras widget init function, served from development url');
-};
-var grasWidget = {
-    init: init
-};
-window.grasWidget = grasWidget;
-console.log('gras widget loaded');
--- packages/delivery-widget/widget/index.ts
+++ packages/delivery-widget/widget/index.ts
@@ -1,5 +1,19 @@
 var init = function () {
-    console.log('this is the gras widget init function, served from development url');
+    function addElement() {
+        // create a new div element
+        const newDiv = document.createElement("div");
+      
+        // and give it some content
+        const newContent = document.createTextNode("A div created with Gras Widget init script!");
+      
+        // add the text node to the newly created div
+        newDiv.appendChild(newContent);
+      
+        // add the newly created element and its content into the DOM
+        const currentDiv = document.getElementById("div1");
+        document.body.insertBefore(newDiv, currentDiv);
+        }
+    addElement();
 };
 var grasWidget = {
     init: init
