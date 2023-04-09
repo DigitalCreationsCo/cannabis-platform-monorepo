a5fce01149d1dfef7ad110128d8a350d47adc161
AndresMB <bmejiadeveloper2@gmail.com>
2023-04-09 07:20:54 -0400

    adding closebutton

--- packages/delivery-widget/src/views/Button.tsx
+++ packages/delivery-widget/src/views/Button.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
+import { checkHrefCheckout } from "../util"
 
 function Button() {
 
@@ -7,12 +9,32 @@ function Button() {
     const handleClick = () => {
         history("/checkout")
     }
+
+    useEffect(() => {
+        checkHrefCheckout()
+    }, [window.location.href])
     
     return (
-        <button onClick={handleClick} className="border absolute bottom-0 right-0 m-2 p-4 rounded-full bg-primary">
-            <h1>Delivery by Gras</h1>
-        </button>
+    <div onClick={handleClick} className="border absolute bottom-0 right-0 m-2 p-4 rounded-full bg-primary">
+        { checkHrefCheckout() ? <div className="flex flex-col items-center">
+            <h1>Delivery by Gras straight to your door.</h1>
+            <p>Click here for delivery checkout!</p>
+            </div> : 
+            <div className="flex flex-row items-center">
+                <h1>Delivery by Gras</h1>
+                <p>now at checkout</p>
+            </div>
+        }
+    </div>
     )
+}
+
+function CloseButton() {
+    return (
+        <div className="items-center flex px-2 bg-light text-primary font-bold rounded-full text-3xl">
+            x
+        </div>
+    );
 }
 
 export default Button
--- packages/delivery-widget/src/views/Checkout.tsx
+++ packages/delivery-widget/src/views/Checkout.tsx
@@ -1,16 +1,9 @@
 import { useEffect, useState } from "react"
+import { checkHrefCheckout } from "../util"
 
 export default function Checkout() {
     const [loading, setLoading] = useState(false)
 
-    function checkHrefCheckout() {
-        const href = window.location.href
-        if (href.includes("checkout")) {
-            return true
-        } else {
-            return false
-        }
-    }
     useEffect(() => {
         checkHrefCheckout()
     }, [window.location.href])
@@ -19,7 +12,6 @@ export default function Checkout() {
         setLoading(true)
         // window.location.href = "https://dispensary.gras.com/checkout"
     }
-
 
     return (
         <div className="border absolute bottom-0 right-0 m-2 p-4 rounded-full bg-primary">
@@ -30,15 +22,28 @@ export default function Checkout() {
                     <p>You are being redirected</p>
                 </div> : 
                 checkHrefCheckout() ? 
-                (<div>
+                (
+                <div className="flex flex-col items-center">
                 <button onClick={handleClick}>Get Delivery by Gras</button>
-                </div>)
+                </div>
+                )
             : (
-            <div>
+            <div className="flex flex-row items-center">
+                <div className="flex flex-col">
                 <p>Gras and 'dispensary' are teaming up to delivery your goods straight to your home. </p>
                 <h1>Click here during checkout to get your order by delivery</h1>
+                </div>
+                <CloseButton />
             </div>
             )}
         </div>
     )
+}
+
+function CloseButton() {
+    return (
+        <div className="cursor-default py-2 text-light text-3xl">
+            x
+        </div>
+    );
 }
