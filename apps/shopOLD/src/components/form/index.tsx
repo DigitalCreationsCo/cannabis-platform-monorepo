8fda0b1c4c2341522abe7d3c985cfc60cad32507
AndresMB <bmejiadeveloper2@gmail.com>
2023-04-12 22:51:48 -0400

    resolve shop build errors

--- apps/shopOLD/src/components/form/index.tsx
+++
@@ -1,5 +0,0 @@
-export { default as DispensaryCreate } from './DispensaryCreate';
-export { default as DispensaryReview } from './DispensaryReview';
-export { default as DispensarySignUpComplete } from './DispensarySignUpComplete';
-export { default as DispensaryUserCreate } from './DispensaryUserCreate';
-
