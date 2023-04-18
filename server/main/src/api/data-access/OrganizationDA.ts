2282bcfa1a953ca55bdc95c966c6bcfefb23df38
AndresMB <bmejiadeveloper2@gmail.com>
2023-04-18 13:01:19 -0400

    api/data-access added

---
+++ server/main/src/api/data-access/OrganizationDA.ts
@@ -0,0 +1,83 @@
+import { urlBuilder } from '@cd/core-lib';
+import { createOrganization, findCategoryListByOrg, findOrganizationById, findUsersByOrganization, OrganizationCreateType } from '@cd/data-access';
+import axios from 'axios';
+/* =================================
+Organization Data Access - data class for organization table
+
+members:
+createOrganization
+getOrganizationById
+getCategoryList
+getUsersByOrganization
+updateProduct
+
+================================= */
+
+export default class OrganizationDA {
+    static async createOrganization(organization: OrganizationCreateType) {
+        try {
+            const data = await createOrganization(organization);
+            await axios.post(urlBuilder.location.createorganizationLocationRecord(), { ...organization },{ headers: {
+                          Accept: "application/json",
+                          "Content-Type": "application/json",
+                        }})
+            console.log(`${organization.name} record is created.`)
+
+            await axios.post(urlBuilder.payment.createDispensaryAccount(), { ...organization },{ 
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                }
+            })
+            console.log('stripe account created.')
+
+            return 'Your organization account is created';
+        } catch (error) {
+            console.error(error.message);
+            throw new Error(error.message);
+        }
+    }
+
+    // find organization by organizationId
+    static async getOrganizationById(organizationId) {
+        try {
+            const data = await findOrganizationById(organizationId);
+            return data;
+        } catch (error) {
+            console.error(error.message);
+            throw new Error(error.message);
+        }
+    }
+
+    // find CategoryList by organizationId, default arg is 1 for platform wide CategoryList
+    static async getCategoryList(organizationId = '1') {
+        try {
+            const data = await findCategoryListByOrg(organizationId);
+            return data;
+        } catch (error) {
+            console.error(error.message);
+            throw new Error(error.message);
+        }
+    }
+
+    static async getUsersByOrganization(organizationId) {
+        try {
+            const data = await findUsersByOrganization(organizationId);
+            return data;
+        } catch (error) {
+            console.error(error.message);
+            throw new Error(error.message);
+        }
+    }
+
+    static async updateProduct(product) {
+        try {
+            return 'TEST: product was updated OK';
+            // const data = await updateProduct(product);
+            // return data
+        } catch (error) {
+            console.error(error.message);
+            throw new Error(error.message);
+        }
+    }
+}
---
+++ server/main/src/api/data-access/SessionDA.ts
@@ -0,0 +1,56 @@
+import { createSession, deleteSessionByHandle, findSessionByHandle, SessionPayload, updateExpireSession } from '@cd/data-access';
+
+/* =================================
+Session Data Access - data class for Session table
+
+members: 
+getSession
+createUserSession
+updateExpireSession
+deleteSession
+
+================================= */
+
+export default class SessionDA {
+    static async getSession(handle:string) {
+        try {
+            const data = await findSessionByHandle(handle);
+            return data;
+        } catch (error) {
+            console.error(error);
+            throw new Error(error);
+        }
+    }
+
+    static async createUserSession(sessionHandle: string, sessionPayload: SessionPayload, expires: number) {
+        try {
+            console.log('creating user session for user: ', sessionPayload)
+            const session = await createSession(sessionHandle, sessionPayload, expires)
+            return session;
+        } catch (error) {
+            console.error(error.message);
+            throw new Error(error);
+        }
+    }
+
+    static async updateExpireSession(handle:string, expires: number) {
+        try {
+            console.log('updating session expire for session: ', handle, expires)
+            const data = await updateExpireSession(handle, expires);
+            return data;
+        } catch (error) {
+            console.error(error);
+            throw new Error(error);
+        }
+    }
+
+    static async deleteSession(handle:string) {
+        try {
+            const data = await deleteSessionByHandle(handle);
+            return data;
+        } catch (error) {
+            console.error(error);
+            throw new Error(error);
+        }
+    }
+}
