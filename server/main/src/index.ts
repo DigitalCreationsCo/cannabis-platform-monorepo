dd123ef8d2416727e86301fd7fad6735c4767293
AndresMB <bmejiadeveloper2@gmail.com>
2023-04-18 13:51:39 -0400

    main-server, move src files into src

--- server/main/src/index.ts
+++ server/main/src/index.ts
@@ -1,5 +1,24 @@
 import prisma from '@cd/data-access';
 import server from "./server";
+// import { loadEnv } from '@cd/shared-config/config/loadEnv';
+// import { config } from 'dotenv';
+// import { expand } from 'dotenv-expand';
+const nodeEnv = process.env.NODE_ENV
+// expand(config({ path: loadEnv(nodeEnv) }));
+const port = process.env.SERVER_MAIN_PORT || 'NO_PORT_FOUND';
+
+connectDb()
+    .then(() => {
+        server.listen(port, () => {
+            console.log(` 🚀 Server Main listening on port ${port}.`);
+            console.info(' ♞ Server Main running in ' + nodeEnv + ' mode.');
+        });
+    })
+    .catch((err) => {
+        console.error('Error connecting to database: ', err.stack);
+        process.exit(1);
+    });
+
 
 async function connectDb() {
   try {
