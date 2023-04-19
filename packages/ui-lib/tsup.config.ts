f8d95cbc55b54785dfa6e793579ccddf459072ba
AndresMB <bmejiadeveloper2@gmail.com>
2023-04-18 22:22:25 -0400

    ui-lib build and watch works OK, removed browser property from tsup config

--- packages/ui-lib/tsup.config.ts
+++ packages/ui-lib/tsup.config.ts
@@ -7,7 +7,6 @@ export default defineConfig((options) => ({
   clean: true,
   dts: true,
   format: ['esm', 'cjs'],
-  platform: 'browser',
   target: ['es2017', 'chrome70', 'edge18', 'firefox70', 'node16'],
   tsconfig: new URL('./tsconfig.build.json', import.meta.url).pathname,
   sourcemap: !options.watch,
