63a00acbfd704b86d8278b0e81b25467921335c0
AndresMB <bmejiadeveloper2@gmail.com>
2023-04-19 22:35:36 -0400

    update data-access tsconfig.build.json

--- packages/data-access/tsup.config.ts
+++ packages/data-access/tsup.config.ts
@@ -1,15 +1,8 @@
+import path from 'path';
 import { defineConfig } from 'tsup';
 
 export default defineConfig((options) => {
-  console.log(' :: database url: ', process.env.DATABASE_URL)
   return {
-    // env: {
-    //   NODE_ENV: process.env.NODE_ENV,
-    //   DATABASE_URL: process.env.DATABASE_URL,
-    //   SHADOW_DATABASE_URL: process.env.SHADOW_DATABASE_URL,
-    //   MONGODB_URL: process.env.MONGODB_URL,
-    //   GEO_DB_NS: process.env.GEO_DB_NS
-    // },
     entry: ['src/index.ts'],
     splitting: true,
     clean: true,
@@ -17,7 +10,7 @@ export default defineConfig((options) => {
     format: ['esm', 'cjs'],
     platform: 'node',
     target: ['node16'],
-    tsconfig: new URL('./tsconfig.build.json', import.meta.url).pathname,
+    tsconfig: path.resolve(__dirname, './tsconfig.build.json'),
     sourcemap: !options.watch,
     // Do not minify node only packages to let patching possible by the consumer (ie: patch-package)
     minify: false,
