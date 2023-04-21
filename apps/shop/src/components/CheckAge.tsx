01a2e12273c23ed5512d0bc14706beeb729174b9
AndresMB <bmejiadeveloper2@gmail.com>
2023-04-21 01:00:53 -0400

    site entry redirect for base url and quick-delivery

---
+++ apps/shop/src/components/CheckAge.tsx
@@ -0,0 +1,37 @@
+import { Button, Center, CheckBox, FlexBox, H1, H3 } from "@cd/ui-lib"
+import { useRouter } from "next/router"
+import { useState } from "react"
+import { useCookies } from "react-cookie"
+
+const CheckAge = ({ redirect }: { redirect?: string }) => {
+    const router = useRouter()
+    const [cookies, setCookie] = useCookies(['yesOver21'])
+
+    const [yesOver21, setYesOver21] = useState(false)
+    const toggleOver21 = () => setYesOver21(!yesOver21)
+    
+    const confirmAgeOver21 = () => {
+        setCookie('yesOver21', 'true')  
+        router.push({ pathname: redirect }) // navigates back to the previous page, or the home page if there is no redirect URL
+    }
+
+    return (
+    <Center>
+        <H1>Welcome to Gras</H1>
+        <H3>
+            first thing first,
+            Are you 21 years or older?
+            </H3>
+            
+        <FlexBox className="p-4 space-x-4 space-y-4">
+
+            <CheckBox onChange={toggleOver21} checked={yesOver21} label="I'm over 21" />
+
+            <Button className="p-4"
+            onClick={confirmAgeOver21}>
+                Continue</Button>
+        </FlexBox>
+    </Center>
+)}
+
+export default CheckAge
--- apps/shop/src/pages/quick-delivery/index.tsx
+++ apps/shop/src/pages/quick-delivery/index.tsx
@@ -1,14 +1,13 @@
-import { Button, Center, FlexBox, H3, Page } from "@cd/ui-lib/components";
+import { Page } from "@cd/ui-lib/components";
 import VerifyPhotoId from "components/VerifyPhotoId";
 import Head from "next/head";
-import Link from "next/link";
 import { useState } from "react";
 
 function QuickDelivery() {
     const [formStep, setFormStep] = useState(0);
     const nextFormStep = () => setFormStep((currentStep) => currentStep + 1);
     // const prevFormStep = () => setFormStep((currentStep) => currentStep - 1);
-    const FormStepComponents = [Age, VerifyPhotoId];    
+    const FormStepComponents = [VerifyPhotoId];    
     const styles = { gradient: ['bg-gradient-to-b', 'from-primary', 'to-secondary', 'p-0 lg:p-16 h-max'] };
     
     return (
@@ -26,14 +25,3 @@ function QuickDelivery() {
 }
 
 export default QuickDelivery
-
-const Age = ({ nextFormStep }: { nextFormStep: () => void }) => (
-    <Center>
-        <H3>Are you 21 years or older?</H3>
-        <FlexBox className="flex-row p-4 space-x-4">
-            <Button className="p-4"
-            onClick={nextFormStep}>Yes</Button>
-            <Link href="/sorry-we-cant-serve-you"><Button className="p-4">No</Button></Link>
-        </FlexBox>
-    </Center>
-)
