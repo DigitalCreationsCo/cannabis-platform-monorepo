44cb0efdaad4fe4b3339a35ae6c9377179b51280
AndresMB <bmejiadeveloper2@gmail.com>
2023-04-24 18:03:12 -0400

    data-access tests are not working, NEED WORK HERE

--- packages/data-access/test/organization.test.ts
+++ packages/data-access/test/organization.test.ts
@@ -2,7 +2,7 @@ import { findOrganizationById } from '../src/organization.js';
 
 test('find organization with the given organizationId', async () => {
     const organization = {
-       ...
+       id:'2'
     };
 
     // prismaMock.organization.findUnique.mockResolvedValue(organization)
