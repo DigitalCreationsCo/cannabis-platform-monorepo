51e82010decbe11f5b620eed5c87bc8fc43d0aef
AndresMB <bmejiadeveloper2@gmail.com>
2023-04-30 04:40:59 -0400

    resolved webpack config, still having a issue with window location and rendering widget

--- apps/delivery-widget/webpack.config.cjs
+++ apps/delivery-widget/webpack.config.cjs
@@ -1,11 +1,13 @@
 module.exports = [
     {
+        mode: 'development',
         entry: './index.ts',
         output: {
             path: __dirname + '/dist',
             publicPath: '/',
             filename: 'index.js'
         },
+        devtool: 'inline-source-map',
         resolve: {
             extensions: [".tsx", ".ts", ".jsx", ".js", ".css"],
         },
@@ -46,57 +48,57 @@ module.exports = [
             ]
         }
     },
-    {
-        entry: './src/index.tsx',
-        output: {
-            path: __dirname + '/dist',
-            publicPath: '/',
-            filename: 'widget.js'
-        },
-        resolve: {
-            extensions: [".tsx", ".ts", ".jsx", ".js", ".css"],
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.(ts|tsx)$/,
-                    exclude: /node_modules/,
-                    use: ["ts-loader"],
-                },
-                {
-                    test: /\.(js|jsx)$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: 'babel-loader',
-                        options: {
-                        presets: ['@babel/preset-env'],
-                        },
-                    },
-                },
-                {
-                    test: /\.(css|scss)$/,
-                    use: [
-                        "style-loader", 
-                        "css-loader",
-                        {
-                            loader: "postcss-loader",
-                            options: {
-                            postcssOptions: {
-                                plugins: [
-                                [
-                                    "postcss-preset-env",
-                                    {
-                                    // Options
-                                    },
-                                ],
-                                ],
-                            },
-                            },
-                        }
-                    ],
-                },
+    // {
+    //     entry: './src/index.tsx',
+    //     output: {
+    //         path: __dirname + '/dist',
+    //         publicPath: '/',
+    //         filename: 'widget.js'
+    //     },
+    //     resolve: {
+    //         extensions: [".tsx", ".ts", ".jsx", ".js", ".css"],
+    //     },
+    //     module: {
+    //         rules: [
+    //             {
+    //                 test: /\.(ts|tsx)$/,
+    //                 exclude: /node_modules/,
+    //                 use: ["ts-loader"],
+    //             },
+    //             {
+    //                 test: /\.(js|jsx)$/,
+    //                 exclude: /node_modules/,
+    //                 use: {
+    //                     loader: 'babel-loader',
+    //                     options: {
+    //                     presets: ['@babel/preset-env'],
+    //                     },
+    //                 },
+    //             },
+    //             {
+    //                 test: /\.(css|scss)$/,
+    //                 use: [
+    //                     "style-loader", 
+    //                     "css-loader",
+    //                     {
+    //                         loader: "postcss-loader",
+    //                         options: {
+    //                         postcssOptions: {
+    //                             plugins: [
+    //                             [
+    //                                 "postcss-preset-env",
+    //                                 {
+    //                                 // Options
+    //                                 },
+    //                             ],
+    //                             ],
+    //                         },
+    //                         },
+    //                     }
+    //                 ],
+    //             },
 
-            ]
-        }
-    }
+    //         ]
+    //     }
+    // }
 ]
