7e931cf8e783fcd331425dcadde96b92051bae01
AndresMB <bmejiadeveloper2@gmail.com>
2023-04-30 02:40:21 -0400

    added manual tailwind styles, its ugly, but it works. I can automate it with a script, or plugin later

--- apps/delivery-widget/postcss.config.js
+++ apps/delivery-widget/postcss.config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
     plugins: {
-        'postcss-namespace': {},
+        'postcss-namespace': {token: ' .'},
         'postcss-import': {},
         tailwindcss: {},
         autoprefixer: {}
