4983a46160f24736f18b75445b4272bfd115961b
AndresMB <bmejiadeveloper2@gmail.com>
2023-04-30 11:11:32 -0400

    set up dispensary testing switch on test-widget-site

--- apps/delivery-widget/src/views/WidgetView.tsx
+++ apps/delivery-widget/src/views/WidgetView.tsx
@@ -17,7 +17,7 @@ const WidgetView = (View:(props:ViewProps) => JSX.Element) => () => {
         function checkPath() {
             if (location.pathname != pathname) {
                 pathname = location.pathname;
-                console.log('is checkout? ', useCheckHrefIncludes('checkout'))
+                // console.log('is checkout? ', useCheckHrefIncludes('checkout'))
                 useCheckHrefIncludes('checkout') ? history('/checkout') : history('/')
             }
         }
