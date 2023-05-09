25a1dfa9e0d88ee361141e3150f894d11e83c504
AndresMB <bmejiadeveloper2@gmail.com>
2023-05-09 17:03:00 -0400

    add false true for checkLegalAgeFromIdImage, for now

--- server/image/src/api/controllers/image.controller.ts
+++ server/image/src/api/controllers/image.controller.ts
@@ -28,8 +28,10 @@ export default class ImageController {
         uploadedImages = await uploadImageToS3ObjectStore(images, process.env.VERIFY_ID_BUCKET_NAME)
         isUploaded = true
         
-        const idFrontImage = getObjectStorageUri(uploadedImages['idFrontImage'], process.env.VERIFY_ID_BUCKET_NAME, process.env.OBJECT_STORAGE_REGION)
-        isLegalAge = await checkLegalAgeFromIdImage(idFrontImage)
+        // const idFrontImage = getObjectStorageUri(uploadedImages['idFrontImage'], process.env.VERIFY_ID_BUCKET_NAME, process.env.OBJECT_STORAGE_REGION)
+        // isLegalAge = await checkLegalAgeFromIdImage(idFrontImage)
+        isLegalAge = true
+
         idVerified = true
         
       } else {
