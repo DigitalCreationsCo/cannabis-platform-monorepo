d3b3adc06a041974492f186542a4536711a39083
AndresMB <bmejiadeveloper2@gmail.com>
2023-05-08 22:10:14 -0400

    refactor upload s3 function, need to test it :)

--- server/image/src/server.ts
+++ server/image/src/server.ts
@@ -4,19 +4,19 @@ import http from 'http';
 import multer from 'multer';
 import { imageCtrl } from './api/controllers';
 
-const imageUpload = multer({
-    limits: {
-        fieldNameSize: 100,
-        fileSize: 2000000, // 2MB
-        fieldSize: 2000000, // 2MB
-    },
-    fileFilter: (req, file, cb) => {
-        if (file.mimetype === "image/png" || file.mimetype === "image/jpeg") {
-            cb(null, true);
-        } else {
-            cb(new Error("Only .png, .jpg and .jpeg format allowed!"));
-        }
-    }
+const upload = multer({
+    // limits: {
+    //     fieldNameSize: 100,
+    //     fileSize: 2000000, // 2MB
+    //     fieldSize: 2000000, // 2MB
+    // },
+    // fileFilter: (req, file, cb) => {
+    //     if (file.mimetype === "image/png" || file.mimetype === "image/jpeg") {
+    //         cb(null, true);
+    //     } else {
+    //         cb(new Error("Only .png, .jpg and .jpeg format allowed!"));
+    //     }
+    // }
 });
 
 const app = express();
@@ -25,7 +25,7 @@ app.use(cors());
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json());
 
-app.post("/api/v1/image/scan-identification-upload", imageUpload.any(), imageCtrl.verifyIdentificationImageFromUpload);
+app.post("/api/v1/image/scan-identification-upload", upload.any(), imageCtrl.verifyIdentificationImageFromUpload);
 
 app.post("/api/v1/image/scan-identification-uri", imageCtrl.verifyIdentificationImageFromUri);
 
