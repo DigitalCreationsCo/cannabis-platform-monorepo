bf92d40a0d229a494c761ea2c95967c446b98718
AndresMB <bmejiadeveloper2@gmail.com>
2023-05-09 11:39:35 -0400

    resolving image upload data stream issue

--- server/image/src/s3/verifyIdClient.ts
+++ server/image/src/s3/verifyIdClient.ts
@@ -1,12 +1,11 @@
-import { S3Client } from "@aws-sdk/client-s3";
+// import { S3Client } from "@aws-sdk/client-s3";
 
-const s3Client = new S3Client({
-  endpoint: process.env.OBJECT_STORAGE_ENDPOINT,
-  region: process.env.OBJECT_STORAGE_REGION,
-  credentials: {
-    accessKeyId: process.env.OSTORE_KEY,
-    secretAccessKey: process.env.OSTORE_SEC,
-  },
-});
+// const s3Client = new S3Client({
+//   region: process.env.OBJECT_STORAGE_REGION,
+//   credentials: {
+//     accessKeyId: process.env.OSTORE_KEY,
+//     secretAccessKey: process.env.OSTORE_SEC,
+//   },
+// });
 
-export default s3Client;
+export default {};
