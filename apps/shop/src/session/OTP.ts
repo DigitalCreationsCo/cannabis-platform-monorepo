0efda8e8c7d577d1d4b1bc1ffe17a6af5b7fc197
AndresMB <bmejiadeveloper2@gmail.com>
2023-05-11 14:18:29 -0400

    sendOTP error handling

--- apps/shop/src/session/OTP.ts
+++ apps/shop/src/session/OTP.ts
@@ -1,6 +1,5 @@
 import { UserWithDetails } from "@cd/data-access";
 import Router from "next/router";
-import { toast } from "react-hot-toast";
 import { consumeCode, createCode, resendCode } from "supertokens-auth-react/recipe/passwordless";
     
 async function sendOTPEmail(email: string) {
@@ -8,14 +7,13 @@ async function sendOTPEmail(email: string) {
         let response = await createCode({
             email
         });
-        toast.success("Please check your email for the one time passcode.");
     } catch (err: any) {
         if (err.isSuperTokensGeneralError === true) {
             // this may be a custom error message sent from the API by you,
             // or if the input email / phone number is not valid.
-            toast.error(err.message);
+            throw new Error(err.message);
         } else {
-            toast.error("Oops! Something went wrong.");
+            throw new Error("Something went wrong. Please try again.");
         }
     }
 }
@@ -26,16 +24,15 @@ async function sendOTPPhone(phoneNumber: string) {
         let response = await createCode({
             phoneNumber
         });
-        toast.success("Please check your mobile messages for the one time passcode.");
     } catch (err: any) {
         if (err.isSuperTokensGeneralError === true) {
             console.error(err)
             // this may be a custom error message sent from the API by you,
             // or if the input email / phone number is not valid.
-            toast.error(err.message);
+            throw new Error(err.message);
         } else {
             console.error(err)
-            toast.error("Oops! Something went wrong.");
+            throw new Error("Something went wrong. Please try again.");
         }
     }
 }
@@ -46,17 +43,14 @@ async function resendOTP() {
         if (response.status === "RESTART_FLOW_ERROR") {
             // this can happen if the user has already successfully logged in into
             // another device whilst also trying to login to this one.
-            toast.error("Login failed. Please try again");
-            window.location.assign("/auth")
-        } else {
-            // OTP resent successfully.
+            throw new Error("Login failed. Please try again");
         }
     } catch (err: any) {
         if (err.isSuperTokensGeneralError === true) {
             // this may be a custom error message sent from the API by you.
-            toast.error(err.message);
+            throw new Error(err.message);
         } else {
-            toast.error("Oops! Something went wrong.");
+            throw new Error("Oops! Something went wrong.");
         }
     }
 }
@@ -108,8 +102,8 @@ async function handleOTPInput(otp: string) {
         if (err.isSuperTokensGeneralError === true) {
             // this may be a custom error message sent from the API by you.
             
+            console.error(err)
             // window.alert(err.message);
-            toast.error(err.message)
             throw new Error(err.message)
         } else {
 
