a32ac40973cfb16e232524efbecab3911f71f985
AndresMB <bmejiadeveloper2@gmail.com>
2023-05-15 09:41:34 -0400

    moved modal components to ui-lib

--- apps/shop/src/components/index.ts
+++ apps/shop/src/components/index.ts
@@ -8,7 +8,6 @@ export { default as DispensaryList } from './DispensaryList';
 export * from './form';
 export { default as LayoutContainer } from './LayoutContainer';
 export { default as LocationProvider } from './LocationProvider';
-export * from './modal';
 export * from "./StepFormProvider";
 export * from './ToastProvider';
 export { default as TopBar } from './TopBar';
--- apps/shop/src/components/modal/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as CheckAgeModal } from './CheckAgeModal';
-export { default as CheckoutModal } from './CheckoutModal';
-export { default as ModalProvider } from './ModalProvider';
-
