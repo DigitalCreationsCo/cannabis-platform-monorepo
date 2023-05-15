5e8961c301ee9ffe60df3d30ce102eefdf564d69
AndresMB <bmejiadeveloper2@gmail.com>
2023-05-15 09:41:34 -0400

    moved modal components to ui-lib

--- packages/ui-lib/src/components/modal/index.tsx
+++ packages/ui-lib/src/components/modal/index.tsx
@@ -1,5 +1,6 @@
 export { default as AddProductModal } from './AddProductModal';
 export { default as CartModal } from './CartModal';
+export { default as CheckAgeModal } from './CheckAgeModal';
 export { default as CheckoutModal } from './CheckoutModal';
 export { default as ConfirmationModal } from './ConfirmationModal';
 export { default as LoginModal } from './LoginModal';
@@ -7,3 +8,4 @@ export * from './Modal';
 export { default as Modal } from './Modal';
 export * from "./ModalProvider";
 export { default as ModalProvider } from './ModalProvider';
+
