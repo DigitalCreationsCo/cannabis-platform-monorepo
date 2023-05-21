8719f96e85b13681d5e3dff6e7fef8a9828b4282
AndresMB <bmejiadeveloper2@gmail.com>
2023-05-21 17:07:56 -0400

    added CheckoutSuccess page

--- apps/shop/src/pages/checkout/success.tsx
+++ apps/shop/src/pages/checkout/success.tsx
@@ -1,13 +1,32 @@
-function CheckoutSuccess() {
-    return (
-        <div>
-            Your checkout is successful!
-            Your order is confirmed, and will be delivered soon!
-            You will receive an email with your order details.
+import { selectCartState } from '@cd/core-lib';
+import { Card, Center, H3, Page, Small } from "@cd/ui-lib";
+import { useState } from 'react';
+import Confetti from "react-confetti";
+import { useSelector } from 'react-redux';
 
-            We will also update you via sms on the status of your order.
-            Thanks for ordering Delivery by Gras!
-        </div>
+function CheckoutSuccess() {
+    const [loadingButton, setLoadingButton] = useState(false);
+    
+    const { order, isLoading } = useSelector(selectCartState)
+    
+    return (
+        <Page>
+            <Confetti />
+            <Card className='m-auto md:max-w-[500px]'>
+            <Center className='space-y-2'>
+                <H3>Thank you for ordering Delivery&nbsp;by&nbsp;Gras.</H3>
+                <Small>From <b>{order.organization.name}</b> and <b>Gras</b>,
+                we appreciate your business and the opportunity to serve our community.</Small>
+
+                <Small>
+                Your order is being sent to a Gras Delivery Person, and will be delivered soon!{'\n'}
+                {/* You will receive an email with your order details. */}
+                <b>Check your sms messages for updates on your order.</b>{'\n'}
+                Thanks for ordering Delivery by Gras.
+                </Small>
+            </Center>
+            </Card>
+        </Page>
     );
 }
 
