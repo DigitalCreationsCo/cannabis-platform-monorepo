4adc6a60ee56503be0b3eff83835c84bf2ede5c3
AndresMB <bmejiadeveloper2@gmail.com>
2023-05-25 02:09:36 -0400

    FormStepProvider works ok

--- apps/shop/src/components/form/DispensarySignUpComplete.tsx
+++ apps/shop/src/components/form/DispensarySignUpComplete.tsx
@@ -3,6 +3,7 @@ import { Address } from '@cd/data-access';
 import { Center, FlexBox, Grid, H2, H4, Paragraph, SignInButton } from '@cd/ui-lib';
 import Image from 'next/image';
 import { useEffect } from 'react';
+import Confetti from 'react-confetti';
 import { useFormContext } from './FormStepProvider';
 
 function DispensarySignUpComplete () {
@@ -18,9 +19,11 @@ function DispensarySignUpComplete () {
         setCanProceed(false);
     }, [])
     return (
-        <Grid className="max-w-[525px] mx-auto space-y-2">
+        <div>
+            <Confetti className='w-full h-full' numberOfPieces={420} recycle={false} />
+        <Grid className="max-w-[525px] mx-auto pt-20 md:pt-8 space-y-2">
             <FlexBox className='flex-row justify-between'>
-                <H2 className='whitespace-normal'>Congratulations, you've created your Dispensary Account with Gras!</H2>
+                <H2 className='whitespace-normal'>Congratulations, you created a Dispensary Account with Gras.</H2>
                 {/* <Paragraph>
                     {`This is a great step towards introducing your products to the Gras Cannabis Marketplace. You're almost ready to meet our customers.
                     Please sign in upload your products and customize your storefront so your new customers can find you in the marketplace. Soon, they can find your products and place orders from your dispensary.
@@ -39,6 +42,7 @@ function DispensarySignUpComplete () {
                 <SignInButton />
             </FlexBox>
         </Grid>
+        </div>
     );
 }
 
