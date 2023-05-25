f9a4b354bafa6c5909982d5682d7a154321d225a
AndresMB <bmejiadeveloper2@gmail.com>
2023-05-25 19:36:06 -0400

    metro config ok

--- mobile/driveForGras/metro.config.js
+++ mobile/driveForGras/metro.config.js
@@ -1,4 +1,24 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
-module.exports = getDefaultConfig(__dirname);
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, '../..');
+
+const config = getDefaultConfig(workspaceRoot);
+
+// const appDependencyPackages = {};
+// config.watchFolders = [projectRoot, ...Object.values(appDependencyPackages)];
+
+config.watchFolders = [workspaceRoot];
+
+// config.resolver.extraNodeModules = appDependencyPackages;
+
+config.resolver.nodeModulesPaths = [
+    path.resolve(projectRoot, 'node_modules'),
+    path.resolve(workspaceRoot, 'node_modules'),
+];
+
+config.resolver.disableHierarchicalLookup = true;
+
+// module.exports = config;
