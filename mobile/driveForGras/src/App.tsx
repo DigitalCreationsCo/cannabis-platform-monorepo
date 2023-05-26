81f51e89294058759ca74208933cf4f77186f728
AndresMB <bmejiadeveloper2@gmail.com>
2023-05-25 20:54:54 -0400

    deleleted src, keep a copy

--- mobile/driveForGras/src/App.tsx
+++ mobile/driveForGras/src/App.tsx
@@ -1,35 +1,35 @@
-import { useFonts } from "expo-font";
-import * as SecureStore from "expo-secure-store";
-import React, { useEffect, useState } from "react";
-import { Provider } from "react-redux";
-import AuthSwitch from "./navigation/AuthSwitch";
-import store from "./redux/store";
-
-import { InitialLoading } from "./screens";
+import React from "react";
+import { Text } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 const App = () => {
-  const [authentication, setAuth] = useState(null);
+  // const [authentication, setAuth] = useState(null);
 
-  useEffect(() => {
-    const storedAuthentication = SecureStore.getItemAsync("authentication")
-      .then((obj) => JSON.parse(obj))
-      .then((obj) => {
-        setAuth(obj);
-      })
-      .catch((error) => console.log(error));
-  }, []);
+  // useEffect(() => {
+  //   const storedAuthentication = SecureStore.getItemAsync("authentication")
+  //     .then((obj) => JSON.parse(obj))
+  //     .then((obj) => {
+  //       setAuth(obj);
+  //     })
+  //     .catch((error) => console.log(error));
+  // }, []);
 
-  const [fontsLoaded] = useFonts({
-    "Rubik-Light": require("./assets/fonts/Rubik-Light.ttf"),
-    "Rubik-Regular": require("./assets/fonts/Rubik-Regular.ttf"),
-    "Rubik-Medium": require("./assets/fonts/Rubik-Medium.ttf"),
-  });
-  if (!fontsLoaded) return <InitialLoading />;
+  // const [fontsLoaded] = useFonts({
+  //   "Rubik-Light": require("./assets/fonts/Rubik-Light.ttf"),
+  //   "Rubik-Regular": require("./assets/fonts/Rubik-Regular.ttf"),
+  //   "Rubik-Medium": require("./assets/fonts/Rubik-Medium.ttf"),
+  // });
+  // if (!fontsLoaded) return <InitialLoading />;
 
   return (
-    <Provider store={store}>
-      <AuthSwitch authentication={authentication} />
-    </Provider>
+    // <Provider store={store}>
+      // {/* <AuthSwitch authentication={authentication} /> */}
+      <SafeAreaProvider>
+        <SafeAreaView>
+      <Text>hello</Text>
+      </SafeAreaView>
+      </SafeAreaProvider>
+    // </Provider>
   );
 };
 
--- mobile/driveForGras/src/util/index.js
+++
@@ -1,2 +0,0 @@
-import { isDestinationNearby } from "./isDestinationNearby";
-export { isDestinationNearby };
