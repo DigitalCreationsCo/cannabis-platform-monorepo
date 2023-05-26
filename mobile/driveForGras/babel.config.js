25a54281bfc471dc45c2820b60a8363edbfd60b1
AndresMB <bmejiadeveloper2@gmail.com>
2023-05-25 20:07:28 -0400

    env vars loaded ok

--- mobile/driveForGras/babel.config.js
+++ mobile/driveForGras/babel.config.js
@@ -1,6 +1,41 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo']
-  };
+// module.exports = function(api) {
+//   api.cache(true);
+//   return {
+//     presets: ['babel-preset-expo'],
+//     plugins: ['transform-inline-environment-variables'],
+//   };
+// };
+
+const path = require("node:path");
+
+module.exports = function (config) {
+    config.cache(true);
+
+    const nodeEnv = process.env.NODE_ENV || 'development';
+
+    return {
+        presets: ['babel-preset-expo', '@babel/preset-typescript'],
+        env: {
+            development: {
+                plugins: [
+                    [
+                        'inline-dotenv',
+                        {
+                            path: path.resolve(__dirname, '../../.env.' + nodeEnv)
+                        }
+                    ]
+                ],
+            },
+            production: {
+                plugins: [
+                    [
+                        'inline-dotenv',
+                        {
+                            path: path.resolve(__dirname, '../../.env.' + nodeEnv)
+                        }
+                    ]
+                ],
+            }
+        }
+    };
 };
