a543691bf6c80b3128c65e1e04789ecc5e9a9ece
AndresMB <bmejiadeveloper2@gmail.com>
2023-05-26 09:03:51 -0400

    cant start appp, native-ui document error

--- mobile/driveForGras/src copy/constants/index.js
+++
@@ -1,9 +0,0 @@
-import Icons from './icons'
-
-import { Logo, Images } from './images'
-
-import { Shadow, Colors, Sizes, Fonts } from './theme'
-
-import { SvgIcons } from './svg'
-
-export { SvgIcons, Icons, Logo, Images, Shadow, Colors, Sizes, Fonts }
--- mobile/driveForGras/src copy/helpers/apiKey.js
+++
@@ -1,4 +0,0 @@
-const apiKeys = {
-  GOOGLE_MAP_API_KEY: () => process.env.GOOGLE_MAP_API_KEY,
-};
-export default apiKeys;
