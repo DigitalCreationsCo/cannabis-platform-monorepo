4ec3f6cf537caa9dcb030ab1ec181624ce7bc355
AndresMB <bmejiadeveloper2@gmail.com>
2023-05-28 19:50:18 -0400

    MapScreen UI OK

--- mobile/driveForGras/src/components/Greeting.tsx
+++ mobile/driveForGras/src/components/Greeting.tsx
@@ -4,18 +4,25 @@ import { View } from "react-native";
 import { useSelector } from "react-redux";
 import { H5 } from "./Typography";
 
-
-export default function Greeting({ isLoading }) {
+type GreetingProps = { 
+  isLoading?: boolean; 
+  subtitle?: string;
+}
+function Greeting ( props: GreetingProps ) {
   
   const 
   { user } = useSelector(selectUserState);
 
   return (
-    <View className='flex flex-row items-center absolute top-0 left-0 z-10 rounded-md p-1 m-1 bg-primary shadow'>
-      <H5 color="light">
-        {' '}Good day{`, ${user.firstName}` || '!'}{" "}
-      </H5>
+    <View className='flex absolute top-0 left-0 z-10 rounded-md p-1 m-1 bg-primary shadow'>
+      <View className="flex-row items-center ">
+        <H5 color="light">
+          {' '}Good day{`, ${user.firstName}` || '!'}{" "}
+        </H5>
         <Icons.Flower color="white"/>
+      </View>
     </View>
   );
 }
+
+export default Greeting;
