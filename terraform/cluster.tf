454bcd60f5e781d48ea04575f99bdd42bc370e20
AndresMB <bmejiadeveloper2@gmail.com>
2023-06-01 03:31:24 -0400

    update gitignore, add kubeconfig

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -5,6 +5,7 @@ resource "local_file" "k8s_config" {
     file_permission = "0600"
 }
 
+# resource 'linode_lke_cluster' from linode provider, named 'terraform_k8s'
 resource "linode_lke_cluster" "terraform_k8s" {
     k8s_version = "${var.k8s_version}"
     label="${var.k8s_label}"
