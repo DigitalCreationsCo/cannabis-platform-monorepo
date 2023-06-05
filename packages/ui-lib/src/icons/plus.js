ca470ba60bc79b1631da9124a437af581e735bc8
AndresMB <bmejiadeveloper2@gmail.com>
2023-06-05 10:58:49 -0400

    delete js icons

--- packages/ui-lib/src/icons/plus.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-const PlusIcon = (props) => {
-    return (_jsxs("svg", { viewBox: "0 0 512 512", ...props, children: [_jsx("title", { children: "Plus Icon" }), _jsx("path", { d: "M224 32v448c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32s-32 14.3-32 32" }), _jsx("path", { d: "M32 288h448c17.7 0 32-14.3 32-32s-14.3-32-32-32H32c-17.7 0-32 14.3-32 32s14.3 32 32 32" })] }));
-};
-export default PlusIcon;
