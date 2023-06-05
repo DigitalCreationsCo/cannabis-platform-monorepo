e5742e94fbcc6456b18f49e45ec0ccf8a81d285b
AndresMB <bmejiadeveloper2@gmail.com>
2023-06-05 01:10:28 -0400

    edit server-main ecosystem.config.js

--- server/main/ecosystem.config.js
+++ server/main/ecosystem.config.js
@@ -1,15 +1,10 @@
-module.exports = (hee) => { 
-
-  console.log('pm2 options ', hee)
-  return {
-    apps: [
-      {
-        name: "server-main",
-        script: ".dist",
-        env_production: {
-          NODE_ENV: "production",
-          PORT: 6001,
-        },
-      },
-    ],
-  }};
+module.exports = [
+  {
+    name: "server-main",
+    script: "dist",
+    env_production: {
+      NODE_ENV: "production",
+      PORT: 6001,
+    },
+  },
+]
