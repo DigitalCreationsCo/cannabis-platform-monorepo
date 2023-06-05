9bfbfa718471034abcdf9dd14a6bcb00ffddbc18
AndresMB <bmejiadeveloper2@gmail.com>
2023-06-05 01:22:50 -0400

    update env.production path

--- server/main/babel.config.js
+++ server/main/babel.config.js
@@ -28,7 +28,7 @@ module.exports = function (config) {
                     [
                         'inline-dotenv',
                         {
-                            path: path.resolve(__dirname, './.env.' + nodeEnv)
+                            path: path.resolve(__dirname, '../../.env.' + nodeEnv)
                         }
                     ]
                 ],
