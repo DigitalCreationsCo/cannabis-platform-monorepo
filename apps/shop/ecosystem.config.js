414dcb58a7cf32c2a4d386ad97f872ab7e9341b0
AndresMB <bmejiadeveloper2@gmail.com>
2023-06-05 13:43:22 -0400

    ecosystem.config.js

--- apps/shop/ecosystem.config.js
+++ apps/shop/ecosystem.config.js
@@ -1,8 +1,6 @@
 module.exports = [
   {
     name: "next-shop",
-    // script: "node .next/standalone/apps/shop/server.js",
-    
     // start script for next standalone build, in ci
     script: "server.js",
     wait_ready: true,
