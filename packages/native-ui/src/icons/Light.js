86a0ec1d4a0b09e0e7ffcc28595ed2f1343d5a91
AndresMB <bmejiadeveloper2@gmail.com>
2023-06-05 11:07:10 -0400

    remove native-ui js icon files

--- packages/native-ui/src/icons/Light.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-const Light = (props) => {
-    return (_jsx("svg", { ...props, viewBox: "0 0 24 24", children: _jsx("path", { d: "M19.0765 9.48063H12.1242L15.5905 0L5 14.5194H11.9522L8.48592 24L19.0765 9.48063Z" }) }));
-};
-export default Light;
