e409acd236c3e9277d3fb4adaf6c386486c4f714
AndresMB <bmejiadeveloper2@gmail.com>
2023-06-05 02:20:11 -0400

    server-main graceful shutdown and waitready pm2

--- server/main/src/index.ts
+++ server/main/src/index.ts
@@ -10,10 +10,12 @@ import server from "./server";
 const port = process.env.SERVER_MAIN_PORT || 'NO_PORT_FOUND';
 
 const mongoConnectUrl = process.env.MONGODB_SERVER_DISPATCH_CLUSTER_URL
+
 connectDb()
     .then(() => {
         server.listen(port, () => {
             console.log(` >> server-main is listening on port ${port}.`);
+            process.send('ready'); // ready signal pm2
         });
     })
     .catch((err) => {
@@ -31,15 +33,23 @@ async function connectDb() {
       await DriverDA.useMongoDB(client)
       console.log(" >> server-main: Mongo Database 👏 is ready for query.");
     })
-    await prisma.$connect()
-    .then(() => {
-      console.log(" >> server-main: Prisma Database 👏👏 is ready for query.")
-    });
-    console.info(" >> server-main is connected to database.");
+    .then(async () => {
+      await prisma.$connect()
+      console.log(" >> server-main: Prisma Database 👏👏 is ready for query.");
+    })
+    .then(() => 
+      console.info(" >> server-main is connected to database."));
   } catch(error:any) {
     console.error(" >> server-main: Error connecting to database: ", error.stack);
     process.exit(1);
   }
 }
 
-export { connectDb, server };
+process.on('SIGINT', async function() {
+  await prisma.$disconnect()
+  .then(process.exit(0))
+  .catch((error:any) => process.exit(1))
+});
+
+  export { connectDb, server };
+
