5150d67b39a4078ae65090ed7792eb35aee1a27f
AndresMB <bmejiadeveloper2@gmail.com>
2023-06-13 12:35:54 -0400

    Page component: gradient prop

--- packages/ui-lib/src/components/Page.tsx
+++ packages/ui-lib/src/components/Page.tsx
@@ -1,14 +1,20 @@
 import { PropsWithChildren } from 'react';
 import { twMerge } from 'tailwind-merge';
 type PageProps = {
-    className?: string;
+    gradient?: 'pink' | 'green';
+    className?: string | string[];
 };
 
-function Page({ className, children }: PageProps & PropsWithChildren) {
-    const styles = {
-        page: ['bg-inverse-soft', 'flex flex-col grow', 'min-w-screen', 'md:pt-8 pb-24', 'lg:px-16']
-    };
-    return <div className={twMerge([styles.page, className])}>{children}</div>;
+function Page ({ gradient, children, className = '' }: PropsWithChildren<PageProps>) {
+    
+    type Styles = (string | string[])[];
+    const styles:Styles = Object.values({
+        page: ['bg-inverse-soft', 'flex flex-col grow', 'min-w-screen', 'md:pt-8 pb-24', 'lg:px-16'],
+        gradient: [gradient && 'anim8-' + gradient + '-gradient' || ''],
+        className
+    });
+
+    return <div className={twMerge(styles)}>{children}</div>;
 }
 
 export default Page;
