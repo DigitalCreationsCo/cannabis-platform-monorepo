54b760ad60ee9d24fa79b3944cf73b73f98e6cdf
AndresMB <bmejiadeveloper2@gmail.com>
2023-06-19 05:09:38 -0400

    updating dashboard components:product row, row. clean up dashboard page, update schema: vartiant rating

--- apps/app/src/components/ProductRow.tsx
+++ apps/app/src/components/ProductRow.tsx
@@ -1,47 +1,57 @@
-import { ProductVariantWithDetails, ProductWithDashboardDetails } from '@cd/data-access';
+import { ProductVariantWithDetails } from '@cd/data-access';
 import { FlexBox, H6, Icons, IconWrapper, Paragraph, Price, Row } from '@cd/ui-lib';
 import Image from 'next/image';
 import Link from 'next/link';
 import { twMerge } from 'tailwind-merge';
+import logo from '../../public/logo.png';
 
-type ProductRowProps = {
-    product: ProductWithDashboardDetails;
-    variant?: ProductVariantWithDetails;
+type ProductVariantRowProps = {
+    variant: ProductVariantWithDetails;
 };
+export default function ProductVariantRow({ variant }: ProductVariantRowProps) {
 
-function ProductRow({ product, variant }: ProductRowProps) {
-    const imageSrc = product?.variants?.[0]?.images?.[0]?.location || variant?.images?.[0]?.location || null;
+    const 
+    imageSrc = variant.images?.[0]?.location || logo;
+
     return (
-        <Link href={`products/${product.id}`}>
+        <Link href={`products/${variant.productId}`}>
             <Row className="h-[44px]">
-                {imageSrc && <Image className="hidden sm:block" src={imageSrc} alt="" width={40} height={40} />}
-                <H6 className="grow">{product.name}</H6>
-                {variant && (
-                    <>
-                        <Paragraph
-                            className={twMerge('flex flex-row justify-center w-max', variant.stock < 6 && 'text-error')}
+                
+                <Image 
+                className="hidden sm:block" 
+                src={imageSrc} 
+                alt={variant.name} 
+                width={40} 
+                height={40} />
+
+                <H6 className="grow">
+                    {variant.name}</H6>
+
+                <Paragraph
+                    className={twMerge(
+                        'flex flex-row justify-center w-max', 
+                        variant.stock < (process.env.LOW_STOCK_THRESHOLD || 7) && 'text-error'
+                        )}
                         >
-                            {variant.stock.toString().padStart(2, '0')} stock
-                        </Paragraph>
-                        <H6 className="flex justify-center w-[80px]">
-                            <Price 
-                            basePrice={variant.basePrice} 
-                            salePrice={variant.salePrice}
-                            discount={variant.discount}
-                            showDiscount={true}
-                            showOriginalPrice={true}
-                            />
-                        </H6>
-                    </>
-                )}
-                <FlexBox>
-                    {/* <IconWrapper Icon={Icons.DottedStar} /> */}
-                    <H6 className="flex justify-center w-[40px]">{product.rating} / 5</H6>
+                    {variant.stock.toString().padStart(2, '0')} in stock
+                </Paragraph>
+                
+                <H6 className="">
+                    <Price className='border'
+                    basePrice={variant.basePrice} 
+                    salePrice={variant.salePrice}
+                    discount={variant.discount}
+                    showDiscount={true}
+                    showOriginalPrice={true}
+                    />
+                </H6>
+
+                <FlexBox className='flex-row'>
+                    <IconWrapper iconColor='yellow' Icon={Icons.StarFilled} />
+                    <H6>{variant.rating} / 5</H6>
                 </FlexBox>
                 <IconWrapper Icon={Icons.Right} />
             </Row>
         </Link>
     );
 }
-
-export default ProductRow;
