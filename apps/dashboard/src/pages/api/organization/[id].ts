b09624c1131530cd69d5b0da4ee037740d7562fb
AndresMB <bmejiadeveloper2@gmail.com>
2023-06-23 13:39:29 -0400

    PRE-remove nextjs multizone in favor of independent apps

--- apps/dashboard/src/pages/api/organization/[id].ts
+++ apps/dashboard/src/pages/api/organization/[id].ts
@@ -1,23 +1,45 @@
 import { urlBuilder } from '@cd/core-lib';
 import axios from 'axios';
-import { authMiddleware, healthCheckMiddleware } from 'middleware';
 import { NextApiRequest, NextApiResponse } from 'next';
 import nc from 'next-connect';
+import NodeCache from 'node-cache';
+
+
+// Notes on caching in directory: /_dev/cache.txt
+const cache = new NodeCache({ stdTTL: 30 });
 
 const handler = nc();
-handler.use(authMiddleware).use(healthCheckMiddleware);
+
+// get a single organization details
 handler.get(async (req: NextApiRequest, res: NextApiResponse) => {
+
     try {
         res.setHeader('Cache-Control', 'public, s-maxage=10, stale-while-revalidate=59');
-        const { id } = req.query;
-        const { data } = await axios(urlBuilder.main.organizationById(id));
-        return res.status(res.statusCode).json(data);
+
+        const
+            { id } = req.query;
+
+        if (cache.has(`organization/${id}`)) {
+
+            const
+                org = cache.get(`organization/${id}`);
+
+            return res.status(200).json(org);
+
+        } else {
+            const
+                { data } = await axios.get(urlBuilder.main.organizationById(id), { validateStatus: status => (status >= 200 && status < 300) || status == 404 });
+
+            cache.set(`organization/${id}`, data);
+
+            return res.status(res.statusCode).json(data);
+
+        }
     } catch (error: any) {
-        console.error(error.message);
-        return res.json(error);
+        console.error('next api getOrganization: ', error.message);
+        return res.status(500).json(error.message);
+
     }
 });
-
-// handler.use(adminMiddleware);
 
 export default handler;
--- apps/dashboard/src/pages/api/organization/index.ts
+++ apps/dashboard/src/pages/api/organization/index.ts
@@ -1,18 +1,38 @@
 import { urlBuilder } from '@cd/core-lib';
 import { OrganizationCreateType } from '@cd/data-access';
 import axios from 'axios';
-import { ExtendRequest, healthCheckMiddleware } from 'middleware';
-import { NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from 'next';
 import nc from 'next-connect';
 
 const handler = nc();
-handler.use(healthCheckMiddleware);
 
-// create organization
-handler.post(async (req: ExtendRequest, res: NextApiResponse) => {
+// create organization, create location record, create stripe account
+handler.post(async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+
+        const
+            organization: OrganizationCreateType = req.body;
+
+        const
+            response = await axios.post(urlBuilder.main.organization(), organization, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+        return res.status(response.status).json(response.data);
+
+    } catch (error: any) {
+        console.error(error.message);
+        return res.json(error);
+    }
+});
+
+// update organization prisma record, update mongodb location record
+handler.put(async (req: NextApiRequest, res: NextApiResponse) => {
     try {
         const formData: OrganizationCreateType = req.body;
-        const { data } = await axios.post(urlBuilder.main.organization(), formData, {
+        const { data } = await axios.put(urlBuilder.main.organization(), formData, {
             headers: {
                 'Content-Type': 'application/json'
             }
