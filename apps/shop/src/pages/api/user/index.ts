6b671c118ce76ee99174a8f9a761b03996f45d88
AndresMB <bmejiadeveloper2@gmail.com>
2023-06-23 19:31:49 -0400

    moved organization admin user apis to dashboard app, deleted from shop

--- apps/shop/src/pages/api/user/index.ts
+++ apps/shop/src/pages/api/user/index.ts
@@ -11,26 +11,6 @@ type ExtendRequest = {}
 
 const cache = new NodeCache({ stdTTL: 20 });
 const handler = nc();
-// handler.use(authMiddleware).use(healthCheckMiddleware);
-// get users from an organization
-handler.get(async (req: ExtendRequest, res: NextApiResponse) => {
-    try {
-        // res.setHeader('Cache-Control', 'public, s-maxage=10, stale-while-revalidate=59');
-        // const { user } = await getSession({ req, res });
-        // const { organizationId } = user.memberships[0];
-        // req.organizationId = organizationId;
-        // if (cache.has(`users/org/${organizationId}`)) {
-        //     const users = cache.get(`users/org/${organizationId}`);
-        //     return res.status(200).json(users);
-        // }
-        // const { data } = await axios(urlBuilder.main.usersByOrg(organizationId));
-        // cache.set(`users/org/${organizationId}`, data);
-        // return res.status(res.statusCode).json(data);
-    } catch (error: any) {
-        console.error(error.message);
-        return res.json(error);
-    }
-});
 
 // create a user record
 handler.post(async (req: NextApiRequest, res: NextApiResponse) => {
