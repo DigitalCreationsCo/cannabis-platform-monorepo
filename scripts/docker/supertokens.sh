1b346af5c15c8d5a867217478c2a7d806cc5a57d
AndresMB <bmejiadeveloper2@gmail.com>
2023-06-24 16:30:43 -0400

    feat: supertokens deployment in kubernetes WIP

--- scripts/docker/supertokens.sh
+++ scripts/docker/supertokens.sh
@@ -2,5 +2,5 @@ docker run \
   --name supertokens-mysql \
   -p 3567:3567 \
   --network=bridge \
-  -e MYSQL_CONNECTION_URI='mysql://4af8mcs8kg8ty8rzyapg:pscale_pw_pxpFbwk2ziXzlthQeoEMkkpr4BuWdgg1aSqBgw6zGqm@aws.connect.psdb.cloud/cannabis_delivery_v1?sslaccept=strict' \
-  -d registry.supertokens.io/supertokens/supertokens-mysql:latest
+  -e MYSQL_CONNECTION_URI='mysql://i6nudrro7avfjzy15vaf:pscale_pw_xUkxaaIiN7evWAYOghNnwZ7nTvXyFKQIuzf5WFF34AW@aws.connect.psdb.cloud/cannabis_delivery_v1?ssl={"rejectUnauthorized":true}' \
+  -d registry.supertokens.io/supertokens/supertokens-mysql
