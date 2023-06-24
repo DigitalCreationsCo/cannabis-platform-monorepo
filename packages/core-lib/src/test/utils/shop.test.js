8ef48e48d3a02a7abee7da5c3594aa25eb6d810b
AndresMB <bmejiadeveloper2@gmail.com>
2023-06-24 12:31:11 -0400

    test: updated checkIsDispensaryOpen test

--- packages/core-lib/src/test/utils/shop.test.js
+++ packages/core-lib/src/test/utils/shop.test.js
@@ -1,16 +1,26 @@
-import { checkDispensaryIsOpen } from '../../utils/schedule';
+import { checkIsDispensaryOpen } from '../../utils/schedule';
 
-describe('checkDispensaryIsOpen', () => {
-    test('is open', async () => {
-        const isOpen = checkDispensaryIsOpen({
-            days: 6543210,
-            id: '1',
-            organizationId: '2',
-            openAt: 8,
-            closeAt: 20,
-            createdAt: new Date(),
-            updatedAt: new Date()
-        })
-        expect(isOpen).toEqual('closed')
+describe('checkIsDispensaryOpen', () => {
+  const now = new Date(),
+    open = new Date(),
+    close = new Date();
+
+  open.setHours(8);
+  close.setHours(20);
+
+  const _isOpenCurrenTime =
+    [6, 5, 4, 3, 2, 1, 0].includes(now.getDay()) && now > open && now < close;
+
+  test('is open', async () => {
+    const isOpen = checkIsDispensaryOpen({
+      days: 6543210,
+      id: '1',
+      organizationId: '2',
+      openAt: 8,
+      closeAt: 20,
+      createdAt: new Date(),
+      updatedAt: new Date(),
     });
+    expect(isOpen).toEqual(_isOpenCurrenTime);
+  });
 });
