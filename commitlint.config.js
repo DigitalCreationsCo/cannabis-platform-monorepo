7ae2e19dbccac4fc1c2103a282af8d10e45e969b
AndresMB <bmejiadeveloper2@gmail.com>
2023-06-24 08:15:25 -0400

    chore: update package.json, yarn.lock

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'dashboard',
+        'shop',
+        'widget',
+        'ui',
+        'core',
+        'data',
+        'main',
+        'location',
+        'dispatch',
+        'image',
+        'payments',
+      ],
+    ],
+  },
 };
