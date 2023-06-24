4d41ee0e98f461269147b288ece03b72639e795b
AndresMB <bmejiadeveloper2@gmail.com>
2023-06-24 06:16:15 -0400

    build: global build successful

--- packages/ui-lib/src/components/PhoneNumber.tsx
+++ packages/ui-lib/src/components/PhoneNumber.tsx
@@ -1,8 +1,12 @@
+export default function PhoneNumber({
+  dialCode,
+  phone,
+}: {
+  dialCode: string;
+  phone: string;
+}) {
+  const hyphenate =
+    phone.slice(0, 3) + '-' + phone.slice(3, 6) + '-' + phone.slice(6, 10);
 
-export default function renderPhoneNumber({ dialCode, phone }: { dialCode: string, phone: string }) {
-    
-    const 
-    hyphenate = phone.slice(0, 3) + '-' + phone.slice(3, 6) + '-' + phone.slice(6, 10);
-
-    return `+${dialCode}-${hyphenate}`;
+  return `+${dialCode}-${hyphenate}`;
 }
