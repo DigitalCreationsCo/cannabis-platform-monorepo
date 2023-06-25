6238f1544938fe1af39c0ec1abe8bf599d705884
AndresMB <bmejiadeveloper2@gmail.com>
2023-06-25 18:51:18 -0400

    ci: tried to implement common cert in dockerfile, no success yet

--- scripts/docker/supertokens.sh
+++ scripts/docker/supertokens.sh
@@ -1,13 +1,7 @@
+# supertokens dev database credentials
+
 docker run \
  -p 3567:3567 \
  --network=bridge \
- -v /etc/ssl/certs/ca-certificates.crt:/etc/ssl/certs/ca-certificates.crt \
- -e MYSQL_CONNECTION_URI="mysql://hjl13pett5q5hdb0b6vs:pscale_pw_Dl9qggUnN02NIVAWDXa19Rsrcv8yCELmjNw1jSaMdb3@aws.connect.psdb.cloud/cannabis_delivery_v1?sslaccept=accept_invalid_certs	" \
+ -e MYSQL_CONNECTION_URI='mysql://i6nudrro7avfjzy15vaf:pscale_pw_xUkxaaIiN7evWAYOghNnwZ7nTvXyFKQIuzf5WFF34AW@aws.connect.psdb.cloud/cannabis_delivery_v1?sslaccept=strict&sslcert=/etc/ssl/certs/ca-certificates.crt' \
  -d registry.supertokens.io/supertokens/supertokens-mysql
-
-# docker run \
-#  -p 3567:3567 \
-#  --network=bridge \
-#  -v /etc/ssl/certs/ca-certificates.crt:/etc/ssl/certs/ca-certificates.crt \
-#  -e MYSQL_CONNECTION_URI="mysql://hjl13pett5q5hdb0b6vs:pscale_pw_Dl9qggUnN02NIVAWDXa19Rsrcv8yCELmjNw1jSaMdb3@aws.connect.psdb.cloud:3306/cannabis_delivery_v1?sslcert=/etc/ssl/certs/ca-certificates.crt&sslaccept=accept_invalid_certs	" \
-#  -d registry.supertokens.io/supertokens/supertokens-mysql
