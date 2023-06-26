fc0ec7b3c9ab512fd10c0b018aad59f5e6520f86
AndresMB <bmejiadeveloper2@gmail.com>
2023-06-26 03:30:59 -0400

    ci(terraform): finalized linkerd terraform.tf, added atlas.tf file

--- terraform/linkerd.tf
+++ terraform/linkerd.tf
@@ -1,34 +1,50 @@
 # note: terraform may fail to install linkerd with helm if namespace is not create in advance
 
 # note: if there is an issue with service account or secrets relating to linkerd, check this issue: https://www.reddit.com/r/devops/comments/gbo3pc/how_to_use_linkerd_with_terraform/
+# run `helm repo add linkerd https://helm.linkerd.io/stable`
+
 resource "helm_release" "linkerd_cni" {
     name        = "linkerd-cni"
 
     namespace   = "linkerd-cni"
-    repository  = "linkerd/linkerd2-cni"
+    repository  = "linkerd"
     chart       = "linkerd2-cni"
-    version     = "30.9.3-edge"
     
     atomic      = true
     create_namespace = true
+    depends_on = [ linode_lke_cluster.terraform_k8s ]
 }
 
 resource "helm_release" "linkerd_crds" {
     name        = "linkerd-crds"
 
     namespace   = "linkerd"
-    repository  = "linkerd/linkerdcrds"
+    repository  = "linkerd"
+    chart       = "linkerd-crds"
+    version     = "1.6.1"
+    create_namespace = true
+
+    set {
+        name    = "cniEnabled"
+        value   = true
+    }
 
     atomic      = true
-    create_namespace = true
+    depends_on = [ helm_release.linkerd_cni ]
 }
 
 resource "helm_release" "linkerd_control_plane" {
     name        = "linkerd-control-plane"
 
     namespace   = "linkerd"
-    repository  = "linkerd/linkerd-control-plane"
+    repository  = "linkerd"
     chart       = "linkerd-control-plane"
+    version     = "1.12.5"
+
+    set {
+        name    = "cniEnabled"
+        value   = true
+    }
 
     set {
         name    = "identityTrustAnchorsPEM"
@@ -45,17 +61,19 @@ resource "helm_release" "linkerd_control_plane" {
         value   = file("${local.root_dir}/issuer.key")
     }
 
-    version     = "2.10.2"
     atomic      = true
+    depends_on = [ helm_release.linkerd_crds ]
 }
 
 resource "helm_release" "linkerd_viz" {
     name        = "linkerd-viz"
 
-    namespace   = "linkerd"
-    repository  = "linkerd/linkerd-viz"
+    namespace   = "linkerd-viz"
+    repository  = "linkerd"
     chart       = "linkerd-viz"
-
     version     = "30.8.5"
+    create_namespace = true
+
     atomic      = true
+    depends_on = [ helm_release.linkerd_control_plane ]
 }
