561396d9e697b0d012aa06a90f648a8f98ee5566
AndresMB <bmejiadeveloper2@gmail.com>
2023-06-27 13:22:29 -0400

    build(dashboard): resolve datetostring import

--- apps/dashboard/src/pages/orders/index.tsx
+++ apps/dashboard/src/pages/orders/index.tsx
@@ -1,83 +1,84 @@
-import { usePagination } from '@cd/core-lib';
+import { dateToString, usePagination } from '@cd/core-lib';
 import { OrderWithDashboardDetails } from '@cd/data-access';
-import { Card, Grid, H6, Icons, OrderRow, Page, PageHeader, Row } from '@cd/ui-lib';
-import { orders, organization, products, userDispensaryAdmin as user } from 'data/dummyData';
-import { dateToString } from 'pages/dashboard';
+import {
+  Card,
+  Grid,
+  H6,
+  Icons,
+  OrderRow,
+  Page,
+  PageHeader,
+  Row,
+} from '@cd/ui-lib';
+import {
+  orders,
+  organization,
+  products,
+  userDispensaryAdmin as user,
+} from 'data/dummyData';
 import { useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 interface OrdersDashboardProps {
-    orders: OrderWithDashboardDetails[];
+  orders: OrderWithDashboardDetails[];
 }
 
 export default function Orders({ orders }: OrdersDashboardProps) {
+  const [currentPage, setCurrentPage] = useState(1);
 
-    const [currentPage, setCurrentPage] = useState(1);
-    
-    const 
-    currentOrders = usePagination(currentPage, orders);
+  const currentOrders = usePagination(currentPage, orders);
 
-    return (
-        <Page className={twMerge("sm:px-4 md:pr-16")}>
+  return (
+    <Page className={twMerge('sm:px-4 md:pr-16')}>
+      <PageHeader title="Orders" Icon={Icons.DeliveryTruck} />
 
-            <PageHeader 
-            title="Orders" 
-            Icon={Icons.DeliveryTruck} 
+      <Grid className="gap-2">
+        <Row className="h-[44px]">
+          <H6 className="w-[100px]">order</H6>
+          <H6 className="grow">status</H6>
+          <H6 className="w-[99px] sm:w-[168px] flex justify-left">
+            date of sale
+          </H6>
+          <H6 className="w-[80px] flex justify-left">total</H6>
+          <div className="hidden sm:block w-[15px]"></div>
+        </Row>
+
+        {currentOrders.length > 0 ? (
+          currentOrders.map((order) => (
+            <OrderRow
+              key={order.id}
+              order={order}
+              orderDetailsRoute="/orders"
             />
-
-            <Grid className='gap-2'>
-                <Row className="h-[44px]">
-                    
-                    <H6 className="w-[100px]">
-                        order</H6>
-                    <H6 className="grow">
-                        status</H6>
-                    <H6 className="w-[99px] sm:w-[168px] flex justify-left">
-                        date of sale</H6>
-                    <H6 className="w-[80px] flex justify-left">
-                        total</H6>
-                    <div className="hidden sm:block w-[15px]"></div>
-                    
-                </Row>
-
-                {currentOrders.length > 0 ? (
-                    currentOrders.map((order) => (
-                        <OrderRow 
-                        key={order.id} 
-                        order={order} 
-                        orderDetailsRoute="/orders" 
-                        />
-                    ))
-                ) : (
-                    <Card>There are no orders.</Card>
-                )}
-                
-            </Grid>
-
-        </Page>
-    );
+          ))
+        ) : (
+          <Card>There are no orders.</Card>
+        )}
+      </Grid>
+    </Page>
+  );
 }
 
 export async function getServerSideProps({ req }: { req: any }) {
-    try {
-        // const orders: Order[] = await (
-        //     await fetch(urlBuilder.dashboard + '/api/orders', {
-        //         headers: {
-        //             Cookie: req.headers.cookie
-        //         }
-        //     })
-        // ).json();
+  try {
+    // const orders: Order[] = await (
+    //     await fetch(urlBuilder.dashboard + '/api/orders', {
+    //         headers: {
+    //             Cookie: req.headers.cookie
+    //         }
+    //     })
+    // ).json();
 
-        return {
-            props: { 
-                user: dateToString(user), 
-                organization: dateToString(organization), 
-                products: dateToString(products) || [], 
-                orders: dateToString(orders) || [], 
-            }
-        }
-    } catch (error: any) {
-        console.log('Orders/[id] SSR error: ', error.message);
-        throw new Error(error);
-    }
+    return {
+      props: {
+        user: dateToString(user),
+        organization: dateToString(organization),
+        products: dateToString(products) || [],
+        orders: dateToString(orders) || [],
+      },
+    };
+  } catch (error: any) {
+    console.log('Orders/[id] SSR error: ', error.message);
+    throw new Error(error);
+  }
 }
