58f5f334d2f73bcc5286e133e35be421ff70b3d1
AndresMB <bmejiadeveloper2@gmail.com>
2023-06-26 23:13:10 -0400

    style(shop): loginModal reponsive, Dashboard app backdrop landing page

--- packages/ui-lib/src/components/Page.tsx
+++ packages/ui-lib/src/components/Page.tsx
@@ -3,32 +3,42 @@ import { twMerge } from 'tailwind-merge';
 import { Tiny } from './Typography';
 
 type PageProps = {
-    gradient?: 'pink' | 'green';
-    className?: string | string[];
+  gradient?: 'pink' | 'green';
+  className?: string | string[];
 };
 
-function Page ({ gradient, children, className = '' }: PropsWithChildren<PageProps>) {
-    
-    const 
-    appVersion = '0.1.0'
-    
-    type Styles = (string | string[])[];
-    const 
-    styles:Styles = Object.values({
-        page: ['bg-inverse-soft', 'flex flex-col grow', 'min-w-screen', 'md:pt-8 pb-24', 'lg:px-16'],
-        gradient: [gradient && 'anim8-' + gradient + '-gradient' || ''],
-        cursor: ['cursor-default'],
-        // hideOverflow: ['overflow-hidden'],
+function Page({
+  gradient,
+  children,
+  className = '',
+}: PropsWithChildren<PageProps>) {
+  const appVersion = '0.1.0';
 
-        className
-    });
+  type Styles = (string | string[])[];
+  const styles: Styles = Object.values({
+    page: [
+      'bg-inverse-soft',
+      'flex flex-col grow',
+      'min-w-screen',
+      'sm:px-4',
+      'md:pt-8 pb-24',
+      'lg:px-16',
+    ],
+    gradient: [(gradient && 'anim8-' + gradient + '-gradient') || ''],
+    cursor: ['cursor-default'],
+    // hideOverflow: ['overflow-hidden'],
 
-    return (
+    className,
+  });
+
+  return (
     <div className={twMerge(styles)}>
-        {children}
-        <Tiny className="fixed bottom-0 right-0 pr-1 cursor-default text-accent-soft">{ appVersion }</Tiny>
+      {children}
+      <Tiny className="fixed bottom-0 right-0 pr-1 cursor-default text-accent-soft">
+        {appVersion}
+      </Tiny>
     </div>
-    );
+  );
 }
 
 export default Page;
