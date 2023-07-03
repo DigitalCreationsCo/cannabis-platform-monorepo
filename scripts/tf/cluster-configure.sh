01bd1bb46c2145c4144fa31905c1a85a809e3bca
AndresMB <bmejiadeveloper2@gmail.com>
2023-07-03 17:50:25 -0400

    ci(cluster): successfully signed tls certificate to secret: gras-cannabis-org

--- scripts/tf/cluster-configure.sh
+++ scripts/tf/cluster-configure.sh
@@ -2,4 +2,8 @@ helm repo update
 helm search repo linkerd2-cni
 helm repo add linkerd https://helm.linkerd.io/stable
 
-terraform -chdir='./terraform' apply
+terraform -chdir='./terraform' apply -auto-approve
+
+kubectl create secret generic clouddns-dns01-solver-svc-acct \
+   --from-file=dns-service-account.json -n cert-manager
+kubectl apply -f k8s
