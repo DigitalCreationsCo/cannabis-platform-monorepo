c39f0324932298bbe83b9751fb635d744a5ca534
AndresMB <bmejiadeveloper2@gmail.com>
2023-07-05 00:09:26 -0400

    feat(shop): react-slick Carousel, added InfoCard, typed CarouselData, type BlogBlurb

---
+++ apps/shop/src/uat/infoCardMock.ts
@@ -0,0 +1,45 @@
+import { BlogBlurb } from 'components/InfoCard';
+
+const _infoCardMockData: BlogBlurb[] = [
+  {
+    id: '1',
+    name: 'your-dispensary-thrives-here',
+    title: `Your Dispensary Thrives Here`,
+    description: 'Blog Description',
+    href: '12345',
+    image: {
+      id: '1',
+      location:
+        'https://greenstocknews.com/images/placeholder/cannabis/medicinal/md1.jpg',
+      blurhash: '',
+    },
+  },
+  {
+    id: '2',
+    name: 'need-delivery-service',
+    title: 'Need Delivery Service',
+    description: 'Blog Description',
+    href: '123457',
+    image: {
+      id: '2',
+      location:
+        'https://img.freepik.com/premium-photo/delivery-person-holding-parcel-with-food-delivery-against-yellow-surface_93675-111653.jpg',
+      blurhash: 'LTMrO.]mvO11}9FZNer_M#odXRnj',
+    },
+  },
+  {
+    id: '3',
+    title: 'Gras Is Here',
+    name: 'gras-is-here',
+    description: 'Blog Description',
+    href: '12377456',
+    image: {
+      id: '3',
+      location:
+        'https://gras-dispensary-images.us-southeast-1.linodeobjects.com/loveweed.png',
+      blurhash: 'LTMrO.]mvO11}9FZNer_M#odXRnj',
+    },
+  },
+];
+
+export { _infoCardMockData };
