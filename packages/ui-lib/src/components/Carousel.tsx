1e558e5382e6fc6b4a9ec4f17b08ff03dda479db
AndresMB <bmejiadeveloper2@gmail.com>
2023-07-05 02:12:52 -0400

    feat(shop): responsive carousel OK

--- packages/ui-lib/src/components/Carousel.tsx
+++ packages/ui-lib/src/components/Carousel.tsx
@@ -1,4 +1,3 @@
-import { useCarousel } from '@cd/core-lib/src/hooks';
 import { useRef } from 'react';
 import Slider, { Settings as CarouselSettings } from 'react-slick';
 import 'slick-carousel/slick/slick-theme.css';
@@ -9,6 +8,10 @@ import { Button } from './button';
 import IconWrapper from './IconWrapper';
 import { H5 } from './Typography';
 
+// TO DO: make responsive showSlides, slidesPresented, and variableWidth
+// clean up interface
+
+
 type CarouselComponent<DataType> = React.FC<{ data: DataType }>;
 
 type CarouselProps<DataType> = CarouselSettings & {
@@ -17,9 +20,7 @@ type CarouselProps<DataType> = CarouselSettings & {
   titleSize?: 'md' | 'lg';
   data: DataType[];
   dataKey: string;
-  axis?: 'horizontal' | 'vertical';
   interval?: number;
-  slidesPresented: number;
   infinite?: boolean;
   run?: boolean;
 };
@@ -30,38 +31,47 @@ export default function Carouse<D>({
   titleSize = 'md',
   data,
   dataKey,
-  axis = 'horizontal', // not implemented for vertical
-  interval = 6000,
-  slidesPresented = 1,
-  variableWidth = true,
-  autoplay,
-  speed = 5000,
+  ...props
 }: CarouselProps<D>) {
+    
   const length = data.length;
-  const numActive = Math.min(length, slidesPresented);
-  const beforeIndices = makeIndices(data.length - 1, -1, numActive);
-  const afterIndices = makeIndices(0, +1, numActive);
-  const [active, setActive, handlers, style] = useCarousel(length, {
-    slidesPresented: numActive,
-    interval,
-  });
+  const slidesToShow = Math.min(length, 4);
 
   var settings: CarouselSettings = {
+    ...props,
+    lazyLoad: 'anticipated',
     arrows: false,
-    dots: false,
     infinite: true,
-    speed: 500,
-    slidesToShow: 2,
+    edgeFriction: 100,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: props.autoplaySpeed || 5000,
+    rows: 1,
+    cssEase: 'ease-out',
+    slidesToShow,
     swipeToSlide: true,
     slidesToScroll: 1,
-    nextArrow: undefined,
-    prevArrow: undefined,
+    adaptiveHeight: true,
+    speed: 500,
+    responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+        ]
   };
 
   const styles = {
-    overFlowContainer: ['relative overflow-hidden'],
-    // container: ['relative cursor-default max-h-20'],
-    container: [''],
+    container: ['lg:px-12 max-h-fit overflow-hidden'],
     title: [
       'flex pt-1 px-8 bottom-0 whitespace-nowrap justify-center',
       'cursor-default',
@@ -70,15 +80,6 @@ export default function Carouse<D>({
       titleSize === 'md' && 'text-lg',
     ],
     control: ['flex space-x-4 justify-center'],
-    carousel: ['overflow-visible', 'relative', 'flex'],
-    content: [
-      'flex',
-      axis === 'horizontal' ? 'flex-row space-x-6' : 'flex-col space-y-6',
-      'overflow-visible',
-      'relative',
-      'min-w-full',
-      'py-4', // for drop shadow
-    ],
     carouselItem: ['max-w-fit carousel-item'],
     carouselButton: [
       'h-full top-0 absolute z-10 min-w-20 px-2',
@@ -93,20 +94,23 @@ export default function Carouse<D>({
     <div className={twMerge(styles.container)}>
       <div className={twMerge(styles.control)}>
         <CarouselButton
-          direction={axis === 'horizontal' ? 'left' : 'up'}
+          direction={settings.vertical ? 'up' : 'left'}
           onClick={sliderRef?.current?.slickPrev}
         />
         <CarouselButton
-          direction={axis === 'horizontal' ? 'right' : 'down'}
+          direction={settings.vertical ? 'down' : 'right'}
           onClick={sliderRef?.current?.slickNext}
         />
       </div>
-      {title && <H5 className={twMerge(styles.title)}>{title}</H5>}
+
+      {title && <H5 className={twMerge(styles.title)}>
+        {title}</H5>}
+        
       <Slider ref={sliderRef} className="w-screen" {...settings}>
         {data.map((el, index) => (
           <div
             key={dataKey + '-container-' + index}
-            className="max-w-fit carousel-item"
+            className="max-w-fit carousel-item m-2"
           >
             <Component key={dataKey + '-' + index} data={el} />
           </div>
@@ -150,7 +154,7 @@ const CarouselButton = ({ direction, onClick }: CarouselButtonProps) => {
     container: ['z-10 relative h-fit', 'hidden', 'md:block'],
     button: [
       'p-4',
-      'h-12 w-20',
+      'h-10 w-20',
       'rounded-lg bg-inverse-soft hover:bg-accent-soft active:bg-accent',
     ],
   };
