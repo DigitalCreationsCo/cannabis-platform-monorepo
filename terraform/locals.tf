ed7987aa6d041ce0338d11181a6bfc8b59079d69
AndresMB <bmejiadeveloper2@gmail.com>
2023-07-06 09:35:08 -0400

    ci(terraform): deploy in us-west4-b zone

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,15 +1,9 @@
 locals {
     # Root directory of the project
     root_dir = "${dirname(abspath(path.root))}"
-
     ca_certificate = file("${local.root_dir}/ca.crt")
     ca_key = file("${local.root_dir}/ca.key")
-
-    ssl_cert_crt = file("${local.root_dir}/ssl.key")
-    ssl_cert_key = file("${local.root_dir}/ssl.crt")
-    
     credentials = file("${local.root_dir}/gcp_service_account.json")
-
     # Path to the kubeconfig directory
     k8s_config_dir = "${local.root_dir}/.kube/"
     # Path to the kubeconfig file
