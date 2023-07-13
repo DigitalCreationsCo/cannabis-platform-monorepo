fbc1e6d61e1136f700842cf68422f18b266f82d0
AndresMB <bmejiadeveloper2@gmail.com>
2023-07-13 15:21:44 -0400

    chore(db): Added supertokens service resouce properties, configured ST in dev and production db, resolved schema errors for postgres provider

--- scripts/docker/supertokens.sh
+++ scripts/docker/supertokens.sh
@@ -1,5 +1,10 @@
 #  Local development image for supertokens
 docker run \
 	-p 3567:3567 \
-	-e MYSQL_CONNECTION_URI="postgresql://postgres:RRJV@y_CK-jz9rj@db.runhqicnmxfwlzwllswi.supabase.co:5432/postgres" \
-	-d registry.supertokens.io/supertokens/supertokens-mysql
+	-e POSTGRESQL_USER=postgres \
+	-e POSTGRESQL_PASSWORD=RRJV@y_CK-jz9rj \
+	-e POSTGRESQL_HOST=db.runhqicnmxfwlzwllswi.supabase.co \
+	-e POSTGRESQL_PORT=5432 \
+	-e POSTGRESQL_DATABASE_NAME=postgres \
+	-e POSTGRESQL_CONNECTION_URI="postgresql://postgres:RRJV@y_CK-jz9rj@db.runhqicnmxfwlzwllswi.supabase.co:5432/postgres" \
+	-d registry.supertokens.io/supertokens/supertokens-postgresql:3.14
