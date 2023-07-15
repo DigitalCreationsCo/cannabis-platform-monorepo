605fcdafc677a2008d0db760fb36f26c5935ed77
AndresMB <bmejiadeveloper2@gmail.com>
2023-07-15 19:42:54 -0400

    Blog page UI draft

--- apps/shop/src/pages/blog/[id].tsx
+++ apps/shop/src/pages/blog/[id].tsx
@@ -1,14 +1,23 @@
 import { TextContent, urlBuilder } from "@cd/core-lib";
 import { ArticleWithDetails } from "@cd/data-access";
-import { FlexBox, H2, H3, LayoutContextProps, Page } from "@cd/ui-lib";
+import { FlexBox, H2, H3, LayoutContextProps, Page, Paragraph, Small } from "@cd/ui-lib";
 import axios from "axios";
+import logo from '../../../public/logo.png';
 
 function BlogArticle ({ blog }: { blog: ArticleWithDetails }) {
+
+    console.info('blog: ', blog);
     return (
-        <Page>
-            <FlexBox>
-                <H3>{TextContent.info.COMPANY_NAME}</H3>
-                <H2>{blog.title}</H2>
+        <Page className="mx-auto">
+            <FlexBox className="grow m-auto">
+                <FlexBox>
+                    <H3>{TextContent.info.COMPANY_NAME}</H3>
+                    <H2>{blog.title}</H2>
+                </FlexBox>
+                <img src={blog.image.location || logo.src } alt={blog.name} className="w-60" />
+                <Paragraph>{ blog.description }</Paragraph>
+                <div className="grow"></div>
+                <Small>{TextContent.info.THANK_YOU} {TextContent.info.MORE_CONTENT_COMING_SOON}</Small>
             </FlexBox>
         </Page>
     );
@@ -18,8 +27,11 @@ export async function getStaticPaths() {
     const 
     response = await axios(urlBuilder.shop + '/api/blog');
 
+    if (response.data.success === false)
+    return { paths: [], fallback: false }
+    
     const
-    blogs = response.data; 
+    blogs = response.data.payload;
     
     const paths =blogs.map((blog: ArticleWithDetails) => ({
         params: { id: blog.id.toString()}
@@ -29,8 +41,9 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }: { params: { id: string }} ) {
     const response = await axios(urlBuilder.shop + `/api/blog/${params.id}`)
+    
     const
-    blog = response.data;
+    blog = response.data.payload;
     return { props: { blog } }
 }
   
