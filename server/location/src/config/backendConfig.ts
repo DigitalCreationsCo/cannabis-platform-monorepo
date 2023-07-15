2c466cabf66c062f3870124edc92ef1b35744c26
AndresMB <bmejiadeveloper2@gmail.com>
2023-07-15 13:21:54 -0400

    fix: backend supertokens config apiDomain

--- server/location/src/config/backendConfig.ts
+++ server/location/src/config/backendConfig.ts
@@ -6,7 +6,7 @@ import { AuthConfig } from '../../interfaces';
 const baseDomain = process.env.NEXT_PUBLIC_APP_DOMAIN || 'localhost';
 const dashboardDomain =
   process.env.NEXT_PUBLIC_DASHBOARD_APP_URL || 'http://localhost:3001';
-const apiDomain = process.env.SERVER_MAIN_URL || `http://localhost:6001`;
+const apiDomain = process.env.NEXT_PUBLIC_SERVER_MAIN_URL || `http://localhost:6001`;
 
 const appInfo = {
   appName: process.env.NEXT_PUBLIC_SHOP_APP_NAME || 'Gras',
