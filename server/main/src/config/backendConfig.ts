660a99b89b0b9aada7345803bf7c00dc75a8f7a8
AndresMB <bmejiadeveloper2@gmail.com>
2023-07-18 12:57:28 -0400

    fix: localhost frontend config api path, healthcheck status and test

--- server/main/src/config/backendConfig.ts
+++ server/main/src/config/backendConfig.ts
@@ -8,14 +8,14 @@ import { AuthConfig } from '../../interfaces';
 import { DriverDA, UserDA } from '../api/data-access';
 
 const baseDomain = process.env.NEXT_PUBLIC_APP_DOMAIN || 'localhost';
-const dashboardDomain =
-  process.env.NEXT_PUBLIC_DASHBOARD_APP_URL || 'http://localhost:3001';
+const dashboardDomain = process.env.NEXT_PUBLIC_DASHBOARD_APP_URL || 'http://localhost:3001';
 const apiDomain = process.env.BACKEND_URL || `http://localhost:6001`;
 
 const appInfo = {
   appName: process.env.NEXT_PUBLIC_SHOP_APP_NAME || 'Gras',
   apiDomain,
   websiteDomain: dashboardDomain,
+  // define this path for auth endpoints
   apiBasePath: '/api/v1',
 };
 
@@ -100,7 +100,6 @@ export const backendConfig = (): AuthConfig => {
                   }
                   return response;
                 } catch (error: any) {
-                  console.info('consumeCode error: ', error.message);
                   return {
                     status: 'RESTART_FLOW_ERROR',
                     message: error.message,
@@ -108,31 +107,6 @@ export const backendConfig = (): AuthConfig => {
                 }
               },
             };
-            // apis: (originalImplementation) => {
-            //     return {
-            //         ...originalImplementation,
-            //         consumeCodePOST: async (input) => {
-            //             if (originalImplementation.consumeCodePOST === undefined) {
-            //                 throw Error("Something went wrong.");
-            //             }
-
-            //             let response = await originalImplementation.consumeCodePOST(input);
-            //             let user
-            //             // Post sign up response, we check if it was successful
-            //             if (response.status === "OK") {
-            //                 let { id, email, phoneNumber } = response.user;
-
-            //                 if (response.createdNewUser) {
-            //                     // TODO: post sign up logic
-            //                 } else {
-            //                     let user = getUserDataFromEmail(email) || null;
-            //                     // TODO: post sign in logicn
-            //                 }
-            //             }
-            //             return response;
-            //         }
-            //     };
-            // }
           },
           apis: (originalImplementation) => {
             return {
@@ -140,26 +114,7 @@ export const backendConfig = (): AuthConfig => {
               consumeCodePOST: async (
                 input: PasswordlessSignInRequestPayload & { options: any }
               ) => {
-                // YES
-                // await input.options.req.getJSONBody();
-
-                // YES
-                // console.info('post input: ' , input.options.req.original.headers);
-
-                // YES
-                // console.info('post input: ' , await input.options.req.getFormData());
-
-                // NO
-                // console.info('post input: ' , await input.options.req.getHeaderValue('appUser'));
-
-                // NO
-                // input.options.req.getHeaderValue('appUser')
-
-                // NO
-                // console.info('post app user? ', input.userContext.appUser)
-
                 const { appUser } = await input.options.req.getJSONBody();
-
                 input.userContext = { ...input.userContext, appUser };
 
                 return originalImplementation.consumeCodePOST(input);
@@ -171,57 +126,9 @@ export const backendConfig = (): AuthConfig => {
       Session.init({
         cookieSecure: true,
         cookieDomain: `.${baseDomain}`,
-        // getTokenTransferMethod: () => "cookie",
-
-        // expose jwt to the frontend, used for websocket authentication
         jwt: {
           enable: true,
         },
-
-        // override: {
-        //                 functions: (originalImplementation) => {
-        //                     return {
-        //                         ...originalImplementation,
-        //                         createNewSession: async (input) => {
-        //                             const userId = input.userContext.id;
-        //                             input.userId = userId;
-        //                             input.accessTokenPayload = { ...input.accessTokenPayload, ...input.userContext };
-
-        //                             const session = await originalImplementation.createNewSession(input);
-
-        //                             const sessionPayload: SessionPayload = {
-        //                                 userId: input.userId,
-        //                                 username: input.accessTokenPayload.username,
-        //                                 email: input.accessTokenPayload.email
-        //                             };
-
-        //                             await SessionDA.createUserSession(
-        //                                 session.getHandle(),
-        //                                 sessionPayload,
-        //                                 await session.getExpiry()
-        //                             );
-
-        //                             return session;
-        //                         }
-        //                     };
-        //                 },
-        //                 apis: (originalImplementation) => {
-        //                     return {
-        //                         ...originalImplementation,
-        //                         refreshPOST: async (input) => {
-        //                             const session = await originalImplementation.refreshPOST(input);
-        //                             console.info('refresh session: ', session);
-        //                             await SessionDA.updateExpireSession(session.getHandle(), await session.getExpiry());
-        //                             return session;
-        //                         },
-        //                         signOutPOST: async (input) => {
-        //                             const response = await originalImplementation.signOutPOST(input);
-        //                             await SessionDA.deleteSession(input.session.getHandle());
-        //                             return response;
-        //                         }
-        //                     };
-        //                 }
-        //             }
       }),
       Dashboard.init({
         apiKey: process.env.SUPERTOKENS_DASHBOARD_KEY,
