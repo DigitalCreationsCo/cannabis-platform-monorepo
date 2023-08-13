9e3a9403e13b6663b7899ef35f78b40aa988f0ea
AndresMB <bmejiadeveloper2@gmail.com>
2023-08-13 00:10:32 -0400

    chore(dashboard): create dispensary reducer, and dispensarymiddleware for handling dashboard app data, in redux state. WIP, TODO: Test, write unit tests

--- app/dashboard/src/components/ProtectedPage.tsx
+++ app/dashboard/src/components/ProtectedPage.tsx
@@ -1,8 +1,7 @@
-import { hasMembershipRoleAccess, selectUserState } from '@cd/core-lib';
+import { selectUserState } from '@cd/core-lib';
 import { useRouter } from 'next/router';
-import { useEffect, type PropsWithChildren } from 'react';
+import { type PropsWithChildren } from 'react';
 import { useSelector } from 'react-redux';
-import LoadingPage from '../pages/LoadingPage';
 
 interface ProtectedPageProps {
 	protectedPages: string[];
@@ -15,30 +14,30 @@ function ProtectedPage({
 	const pageIsProtected = protectedPages.indexOf(router.pathname) !== -1;
 	const user = useSelector(selectUserState);
 
-	useEffect(() => {
-		if (
-			router.pathname === '/' &&
-			hasMembershipRoleAccess(user.user, 'MEMBER')
-		) {
-			router.push('/dashboard');
-		}
-	}, [router]);
+	// useEffect(() => {
+	// 	if (
+	// 		router.pathname === '/' &&
+	// 		hasMembershipRoleAccess(user.user, 'MEMBER')
+	// 	) {
+	// 		router.push('/dashboard');
+	// 	}
+	// }, [router]);
 
-	useEffect(() => {
-		if (pageIsProtected && !hasMembershipRoleAccess(user.user, 'MEMBER')) {
-			router.push('/');
-		}
-	}, [
-		user.isLoading,
-		hasMembershipRoleAccess(user.user, 'MEMBER'),
-		pageIsProtected,
-	]);
+	// useEffect(() => {
+	// 	if (pageIsProtected && !hasMembershipRoleAccess(user.user, 'MEMBER')) {
+	// 		router.push('/');
+	// 	}
+	// }, [
+	// 	user.isLoading,
+	// 	hasMembershipRoleAccess(user.user, 'MEMBER'),
+	// 	pageIsProtected,
+	// ]);
 
-	if (
-		user.isLoading ||
-		(pageIsProtected && !hasMembershipRoleAccess(user.user, 'MEMBER'))
-	)
-		return <LoadingPage />;
+	// if (
+	// 	user.isLoading ||
+	// 	(pageIsProtected && !hasMembershipRoleAccess(user.user, 'MEMBER'))
+	// )
+	// 	return <LoadingPage />;
 
 	return <>{children}</>;
 }
