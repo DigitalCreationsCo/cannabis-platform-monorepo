e2002189e6fc30616e68b6816fb24b3c60bd8c65
Bryant Mejia <bryantmejia@grascannabis.org>
2023-08-17 17:12:18 +0000

    fix(main): backend config, handle membership only for existing user

--- scripts/apply-deployment.sh
+++ scripts/apply-deployment.sh
@@ -1,20 +1,23 @@
-if [ -z "$1" ]; then
-    echo "No deployment supplied"
+if [ -z "$APPLY_FILE" ]; then
+    echo "No deployment file supplied"
     exit 1
 fi
 
-dir="$( cd "$( dirname "$1" )" && pwd )"
-deployment_file="${1##*/}"
+dir="$( cd "$( dirname "$APPLY_FILE" )" && pwd )"
+deployment_file="${APPLY_FILE##*/}"
+echo " input file: $deployment_file"
 modified_deployment_file="modified-${deployment_file}"
 TAG=${TAG:-latest}
 
-echo "TAG is ${TAG}"
+echo " tag is ${TAG}"
 # Replace the placeholder tag value
 sed -e "s|TAG|$TAG|g" "${dir}/${deployment_file}" > "${dir}/${modified_deployment_file}"
 
 # Apply the modified configuration using kubectl
-echo "Applying ${modified_deployment_file} with tag:${TAG}"
+echo " applying ${modified_deployment_file} with tag:${TAG}"
 kubectl apply -f "${dir}/${modified_deployment_file}"
 
 # remove the temporary file
+echo " removing ${modified_deployment_file}"
 rm "${dir}/${modified_deployment_file}"
+echo " done"
