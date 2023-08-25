15158a44689d0c87787e3553ff27ea2ef025198b
AndresMB <bmejiadeveloper2@gmail.com>
2023-08-25 19:27:00 -0400

    chore(widget): typed widget buildcartitems function, unit tests passing OK, KEEP GOING

--- app/checkout-widget/src/crawler/cheerio.test.ts
+++ app/checkout-widget/src/crawler/cheerio.test.ts
@@ -1,5 +1,9 @@
+/* eslint-disable jest/no-commented-out-tests */
 /* eslint-disable @typescript-eslint/naming-convention */
-import { buildCartItems, buildSimpleCart } from './crawler-helpers';
+import * as cheerio from 'cheerio';
+import { buildCartItems, buildSimpleCart } from './cheerio';
+import { Config } from './config';
+import { html } from './data/html-test-data';
 
 // 		// returns null, need to mock the dom, in a checkout component test
 describe('getDomElements: Cart', () => {
@@ -25,15 +29,19 @@ describe('getDomElements: Cart', () => {
 	// 		// });
 	// 	});
 
+	// refactor to use Crawler Class
 	it('buildSimpleCart returns expected object', async () => {
 		const result = {
 			items: [],
 			total: 'string',
 		};
-		expect(buildSimpleCart(result)).toBeDefined();
+		const config = new Config('cart').config;
+		const $ = (selector: any) => document.querySelector(selector);
+		expect(buildSimpleCart(result, config, $)).toBeDefined();
 	});
 
-	it('buildCartItems returns expected object', async () => {
+	// refactor to use Crawler Class
+	it('buildCartItems returns expected object using cheerio', async () => {
 		const result = {
 			'cart-item': [
 				{
@@ -49,6 +57,29 @@ describe('getDomElements: Cart', () => {
 			],
 			total: 'string',
 		};
-		expect(buildCartItems(result['cart-item'])).toBeDefined();
+		const config = new Config('cart').config;
+		const $ = cheerio.load(html);
+		expect(buildCartItems(result['cart-item'], config, $)).toBeDefined();
+	});
+
+	it('buildCartItems returns expected object using document selector', async () => {
+		const result = {
+			'cart-item': [
+				{
+					name: 'something',
+					label: 'string',
+					basePrice: 'string',
+					quantity: 'string',
+					size: 'string',
+					unit: 'string',
+					image: 'string',
+					'cart-item': 'string',
+				},
+			],
+			total: 'string',
+		};
+		const config = new Config('cart').config;
+		global.document = html as unknown as Document;
+		expect(buildCartItems(result['cart-item'], config)).toBeDefined();
 	});
 });
