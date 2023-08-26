395e265b22b3b722f685f09275d53e9862e41887
AndresMB <bmejiadeveloper2@gmail.com>
2023-08-25 20:18:19 -0400

    chore(widget): added tests to cheerio.test, processcrawlerdata test, buildcartitems test

--- app/checkout-widget/src/crawler/cheerio.test.ts
+++ app/checkout-widget/src/crawler/cheerio.test.ts
@@ -1,36 +1,61 @@
 /* eslint-disable jest/no-commented-out-tests */
 /* eslint-disable @typescript-eslint/naming-convention */
+import { type SimpleCart } from '@cd/core-lib/src/types/redux.types';
+import { isArray } from '@cd/core-lib/src/utils/object.util';
 import * as cheerio from 'cheerio';
-import { buildCartItems, buildSimpleCart } from './cheerio';
+import {
+	buildCartItems,
+	buildSimpleCart,
+	getCartDOMElements,
+	processCrawlerData,
+} from './cheerio';
 import { Config } from './config';
 import { html } from './data/html-test-data';
 
 // 		// returns null, need to mock the dom, in a checkout component test
-describe('getDomElements: Cart', () => {
-	// 	it('Crawler returns valid dom elements for the cart', async () => {
-	// 		const DOMKEY = 'cart';
-	// 		const config = new Config(DOMKEY).config;
-	// 		expect(await getDOMElementsFromConfig(config)).not.toBeNull();
+describe('Crawler : Cart', () => {
+	it('returns defined data', async () => {
+		const config = new Config('cart').config;
+		const $ = cheerio.load(html);
+		expect(await getCartDOMElements(config, $)).not.toBeNull();
+	});
 
-	// 		// expect(await getDOMElementsFromConfig(config)).toMatchObject<
-	// 		// 	DOMDataSet[DOMKey]
-	// 		// >({
-	// 		// 	'cart-item': {
-	// 		// 		name: 'something',
-	// 		// 		label: 'string',
-	// 		// 		basePrice: 'string',
-	// 		// 		quantity: 'string',
-	// 		// 		size: 'string',
-	// 		// 		unit: 'string',
-	// 		// 		image: 'string',
-	// 		// 		'cart-item': 'string',
-	// 		// 	},
-	// 		// 	total: 'string',
-	// 		// });
-	// 	});
+	it('returns expected results', async () => {
+		const config = new Config('cart').config;
+		const $ = cheerio.load(html);
 
-	// refactor to use Crawler Class
-	it('buildSimpleCart returns expected object', async () => {
+		const result = await getCartDOMElements(config, $);
+		expect(await getCartDOMElements(config, $)).toMatchObject(result);
+
+		expect(result.items).toBeDefined();
+		expect(isArray(result.items)).toBe(true);
+		expect(result.items.length).toBeGreaterThan(0);
+
+		expect(result.total).toBeDefined();
+		expect(typeof result.total).toBe('string');
+	});
+
+	it('processCrawlerData returns expected data', async () => {
+		const config = new Config('cart').config;
+		const data = await processCrawlerData(html, config, 'cart');
+		const simpleCart: SimpleCart = {
+			total: 0,
+			cartItems: [],
+		};
+		expect(typeof data).toBeDefined();
+		expect(typeof data).toBe('object');
+		expect(typeof data).toStrictEqual(typeof simpleCart);
+
+		expect(data.cartItems).toBeDefined();
+		expect(isArray(data.cartItems)).toBe(true);
+		expect(data.cartItems.length).toBeGreaterThan(0);
+
+		expect(data.total).toBeDefined();
+		expect(data.total).not.toBeNaN();
+		expect(typeof data.total).toBe('number');
+	});
+
+	it('buildSimpleCart returns defined data', async () => {
 		const result = {
 			items: [],
 			total: 'string',
@@ -40,8 +65,7 @@ describe('getDomElements: Cart', () => {
 		expect(buildSimpleCart(result, config, $)).toBeDefined();
 	});
 
-	// refactor to use Crawler Class
-	it('buildCartItems returns expected object using cheerio', async () => {
+	it('buildCartItems returns using cheerio', async () => {
 		const result = {
 			'cart-item': [
 				{
@@ -62,7 +86,7 @@ describe('getDomElements: Cart', () => {
 		expect(buildCartItems(result['cart-item'], config, $)).toBeDefined();
 	});
 
-	it('buildCartItems returns expected object using document selector', async () => {
+	it('buildCartItems returns using document selector', async () => {
 		const result = {
 			'cart-item': [
 				{
