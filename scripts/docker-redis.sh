5049b17a7d102b8994f98b708c647598539805b1
AndresMB <bmejiadeveloper2@gmail.com>
2023-08-30 16:13:09 -0400

    chore: preparing redis monorepo functionality for hsop, dispatch

--- scripts/docker-redis.sh
+++ scripts/docker-redis.sh
@@ -1,2 +1,21 @@
-docker run --name dispatch-publish-redis -d -p 6379:6379 redis redis-server --save 60 1 --loglevel warning
-docker run --name dispatch-connect-redis -d -p 6380:6379 redis redis-server --save 60 1 --loglevel warning
+#!/bin/sh
+
+if [ "$1" = "shop" ]; then
+    echo "Running shop-redis-db"
+    docker run --name shop-redis -d -p 6300:6379 redis redis-server --save 60 1 --loglevel warning
+    exit 0
+elif [ "$1" = "dispatch" ]; then
+    echo "Running dispatch-redis-db"
+    docker run --name dispatch-connect-redis -d -p 6302:6379 redis redis-server --save 60 1 --loglevel warning
+    docker run --name dispatch-publish-redis -d -p 6303:6379 redis redis-server --save 60 1 --loglevel warning
+    exit 0
+elif [ "$1" = "all" ]; then
+    echo "Running all redis db"
+    docker run --name shop-redis -d -p 6300:6379 redis redis-server --save 60 1 --loglevel warning
+    docker run --name dispatch-connect-redis -d -p 6302:6379 redis redis-server --save 60 1 --loglevel warning
+    docker run --name dispatch-publish-redis -d -p 6303:6379 redis redis-server --save 60 1 --loglevel warning
+    exit 0
+else
+    echo "Invalid argument. Usage: $0 shop|dispatch|all"
+    exit 1
+fi
