d2e8f450f1ad34c4c10023d148f97a001723bf3f
AndresMB <bmejiadeveloper2@gmail.com>
2023-12-11 23:08:11 -0500

    fix(blog): generate-preview-image debug sharp input error

--- app/blog/src/pages/api/generate-preview-image.ts
+++ app/blog/src/pages/api/generate-preview-image.ts
@@ -3,19 +3,27 @@ import logo from '../../../public/logo.png';
 
 // const redisUrl = redis://default:*******@redis-14623.c238.us-central1-2.gce.cloud.redislabs.com:xxxxx
 const redisUrl = `redis://default:${process.env.REDIS_IMAGE_PREVIEW_PASSWORD}@${process.env.REDIS_IMAGE_PREVIEW}:${process.env.REDIS_IMAGE_PREVIEW_PORT}`;
+
 // Create the client
 const client = createImageClient({
-	dataset: 'production',
-	projectId: process.env.SANITY_PROJECT_ID,
+	dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+	projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
 	redisUrl,
-	token: process.env.SANITY_READ_WRITE_TOKEN,
+	token: process.env.SANITY_API_EDITOR_TOKEN,
 });
 
 export default async function generatePreviewImage(req, res) {
 	// Generate the image when Sanity's webhook hits your API
 	const { imageUrl, text, _id, _type } = req.body;
 	try {
-		await client.generateImage({
+		console.info(
+			'request generating image for',
+			_type,
+			_id,
+			'imageUrl',
+			imageUrl,
+		);
+		const generated = await client.generateImage({
 			id: _id,
 			backgroundImageUrl: imageUrl,
 			text,
@@ -24,8 +32,11 @@ export default async function generatePreviewImage(req, res) {
 			logo,
 			logoPosition: 'bottomRight',
 		});
+		console.info('generated image for', _id, _type, 'generated', generated);
+		console.info('generated image for', _id, _type);
 		res.status(200).send('Ok');
 	} catch (e) {
+		console.error('Error generating image for', _type, _id, e.message);
 		res.status(500).send(e);
 	}
 }
