3d3bcd9a5baa9f2d257480ee204583f867dace8d
AndresMB <bmejiadeveloper2@gmail.com>
2023-12-17 17:26:45 -0500

    fix(core): change target to node18

--- packages/core-lib/tsup.config.ts
+++ packages/core-lib/tsup.config.ts
@@ -5,6 +5,6 @@ export default defineConfig((options) => ({
 	...options,
 	entry: ['src/index.ts'],
 	format: ['cjs', 'esm'],
-	target: ['es2015', 'node16'],
+	target: ['es2015', 'node18'],
 	tsconfig: path.resolve('./tsconfig.build.json'),
 }));
