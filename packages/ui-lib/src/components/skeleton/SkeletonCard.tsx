fb85abbace47e19414572b67248d0fe405e439ce
AndresMB <bmejiadeveloper2@gmail.com>
2023-12-26 16:51:15 -0500

    commit

--- packages/ui-lib/src/components/skeleton/SkeletonCard.tsx
+++ packages/ui-lib/src/components/skeleton/SkeletonCard.tsx
@@ -4,7 +4,7 @@ import Card from '../Card';
 function SkeletonCard({ className }: { className: string | string[] }) {
 	return (
 		<Card className={twMerge('!px-4 animate-pulse', className)}>
-			<div className="animate-pulse">🌿💚</div>
+			<div className="animate-pulse"></div>
 		</Card>
 	);
 }
