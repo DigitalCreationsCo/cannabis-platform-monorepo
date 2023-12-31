a036cb564a8779010422a810636f825334b4a7df
AndresMB <bmejiadeveloper2@gmail.com>
2023-12-31 04:30:15 -0500

    chore: ssl script: create-update-cert

--- terraform/scripts/cert-scripts/create-update-cert.sh
+++ terraform/scripts/cert-scripts/create-update-cert.sh
@@ -1,7 +1,7 @@
 private_key_file="terraform/certs/privkey.pem"
 certificate_file="terraform/certs/fullchain.pem"
 
-echo "This command does not work. Please see the file terraform/scripts/cert-commands for the correct commands to run."
+echo "This command does not work. Please see the file terraform/scripts for the correct commands to run."
 # echo "Generate new cert request file"
 # echo "with private key ${private_key_file}"
 # openssl req -new -key $private_key_file -out new_cert.csr
