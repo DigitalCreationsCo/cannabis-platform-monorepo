cb55343febc5ebc5bc948b665daceac4e14c8941
AndresMB <bmejiadeveloper2@gmail.com>
2023-12-31 04:28:52 -0500

    chore(main): update supertokens docker script, server/main pingSupertoken init function

--- scripts/supertokens-dev.sh
+++ scripts/supertokens-dev.sh
@@ -6,5 +6,4 @@ docker run \
 	-e POSTGRESQL_HOST=db.runhqicnmxfwlzwllswi.supabase.co \
 	-e POSTGRESQL_PORT=6543 \
 	-e POSTGRESQL_DATABASE_NAME=postgres \
-	-e POSTGRESQL_CONNECTION_URI="postgres://postgres:RRJV@y_CK-jz9rj@db.runhqicnmxfwlzwllswi.supabase.co:6543/postgres?pgbouncer=true&connection_limit=30" \
 	-d registry.supertokens.io/supertokens/supertokens-postgresql:7.0
