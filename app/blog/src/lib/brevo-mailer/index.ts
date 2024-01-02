9beddf5f92137683b8fd3280b5632da0ae3f8c8b
AndresMB <bmejiadeveloper2@gmail.com>
2024-01-02 07:40:21 -0500

    feat: generate-newsletter-route works OK, WIP

--- app/blog/src/lib/brevo-mailer/index.ts
+++ app/blog/src/lib/brevo-mailer/index.ts
@@ -1,4 +1,5 @@
 import * as brevo from '@getbrevo/brevo';
+import { buildNewsletterTemplate } from './newsletter-template';
 
 export class BrevoMailer {
 	private apiInstance: brevo.EmailCampaignsApi;
@@ -18,58 +19,66 @@ export class BrevoMailer {
 			link: string;
 		}[],
 	) {
-		const formattedContent = content.map((item) => {
-			console.info('item: ', item);
-			return `<div>
-                    <h4>${item.title}</h4>
-                    <img src="${item.mainImage}" alt="${
-				item.title
-			}" height="500" width="1000" />
-                    <p>${item.excerpt}</p>
-                    <p>${
-											item.footer?.replace(/\s*here$/, '') ||
-											'More info available '
-										} <a href="${item.link}">here</a></p>
-                </div>
-                <br />
-                <br />`;
-		});
-		const newsLetterEmailCampaign = new brevo.CreateEmailCampaign();
+		try {
+			// const formattedContent = content.map((item) => {
+			// 	console.info('item: ', item);
+			// 	return `<div>
+			//             <h4>${item.title}</h4>
+			//             <img src="${item.mainImage}" alt="${
+			// 		item.title
+			// 	}" height="500" width="1000" />
+			//             <p>${item.excerpt}</p>
+			//             <p>${
+			// 									item.footer?.replace(/\s*here$/, '') ||
+			// 									'More info available '
+			// 								} <a href="${item.link}">here</a></p>
+			//         </div>
+			//         <br />
+			//         <br />`;
+			// });
+			const newsLetterEmailCampaign = new brevo.CreateEmailCampaign();
 
-		newsLetterEmailCampaign.name = subject;
-		newsLetterEmailCampaign.subject = subject;
-		newsLetterEmailCampaign.header = `Weekly Cannabis News from Gras`;
-		newsLetterEmailCampaign.previewText = `Weekly Cannabis News from Gras`;
+			newsLetterEmailCampaign.name = subject;
+			newsLetterEmailCampaign.subject = subject;
+			newsLetterEmailCampaign.header = `Weekly Cannabis News from Gras`;
+			newsLetterEmailCampaign.previewText = `Weekly Cannabis News from Gras`;
 
-		newsLetterEmailCampaign.sender = {
-			name: 'Bryant',
-			email: 'bryantmejia@grascannabis.org',
-		};
+			newsLetterEmailCampaign.sender = {
+				name: 'Bryant',
+				email: 'bryantmejia@grascannabis.org',
+			};
 
-		// newsLetterEmailCampaign.templateId = 15;
+			// newsLetterEmailCampaign.templateId = 15;
 
-		// newsLetterEmailCampaign.recipients = { listIds: [3] };
-		newsLetterEmailCampaign.toField = 'bryantmejia@grascannabis.org';
+			// newsLetterEmailCampaign.recipients = { listIds: [3] };
+			newsLetterEmailCampaign.toField = 'bryantmejia@grascannabis.org';
 
-		// newsLetterEmailCampaign.scheduledAt = new Date().toISOString();
+			// newsLetterEmailCampaign.scheduledAt = new Date().toISOString();
 
-		newsLetterEmailCampaign.htmlContent = `<html><body><h1>This email is sent using brevo Api.</h1>
-        ${formattedContent}</body></html>`;
+			newsLetterEmailCampaign.htmlContent = buildNewsletterTemplate(
+				subject,
+				content,
+			);
 
-		// newsLetterEmailCampaign.params = {
-		// 	parameter: 'My param value',
-		// 	subject: 'common subject',
-		// };
+			// newsLetterEmailCampaign.params = {
+			// 	parameter: 'My param value',
+			// 	subject: 'common subject',
+			// };
 
-		this.apiInstance.createEmailCampaign(newsLetterEmailCampaign).then(
-			function (data) {
-				console.info(
-					'API called successfully. Returned data: ' + JSON.stringify(data),
-				);
-			},
-			function (error) {
-				console.error('brevo.createEmailCampaign: ', error);
-			},
-		);
+			this.apiInstance
+				.createEmailCampaign(newsLetterEmailCampaign)
+				.then((data) => {
+					console.info(
+						'API called successfully. Returned data: ' + JSON.stringify(data),
+					);
+				})
+				.catch((error) => {
+					console.error(error);
+					throw new Error(error);
+				});
+		} catch (error) {
+			console.error('brevo.createEmailCampaign: ', error.message);
+			throw new Error(error.message);
+		}
 	}
 }
