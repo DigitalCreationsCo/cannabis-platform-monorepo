042910a2b8135c39c6170150bb52a306d28c13bb
AndresMB <bmejiadeveloper2@gmail.com>
2024-01-04 14:20:31 -0500

    feat(core): add unsplash API utils, unsplash env variables

---
+++ packages/core-lib/src/utils/unsplash.util.ts
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { axios } from '../axiosInstance';
+
+export async function searchUnsplashPhotoByKeyword(
+	searchTerms: string,
+): Promise<any[]> {
+	try {
+		const response = await axios(
+			`${process.env.UNSPLASH_URL}/search/photos?query=${searchTerms}&orientation=squarish&per_page=1`,
+			{
+				headers: {
+					Authorization: `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`,
+					'Accept-Encoding': 'gzip,deflate,compress',
+				},
+			},
+		);
+
+		console.info('results: ', response.data.results);
+		return response.data.results;
+	} catch (error) {
+		console.error('searchUnsplashPhotoByKeyword: ', error.message);
+		throw new Error(error.message);
+	}
+}
+
+export async function triggerUnsplashDownload(
+	downloadLocationEndpoint: string,
+) {
+	try {
+		const response = await axios(downloadLocationEndpoint, {
+			headers: {
+				Authorization: `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`,
+				'Accept-Encoding': 'gzip,deflate,compress',
+			},
+		});
+		console.info('triggerUnsplashDownload: ', response.data);
+		return response.data;
+	} catch (error) {
+		console.error('triggerUnsplashDownload: ', error.message);
+		throw new Error(error.message);
+	}
+}
