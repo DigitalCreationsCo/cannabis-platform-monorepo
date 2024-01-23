b390e57ebf4e89376bb3ce8900570e13a26a5f46
AndresMB <bmejiadeveloper2@gmail.com>
2024-01-23 09:09:25 -0500

    feat(main): added scheduled generate newsletter cron-job, NEED TO TEST

---
+++ packages/core-lib/lib/brevo-mailer/index.ts
@@ -0,0 +1,170 @@
+/* eslint-disable sonarjs/no-duplicate-string */
+import {
+	type UserWithDetails,
+	type Organization,
+	type DriverWithDetails,
+} from '@cd/data-access';
+import * as brevo from '@getbrevo/brevo';
+import { buildNewsletterTemplate } from './newsletter-template';
+
+const _BryantEmail = 'bryantmejia@grascannabis.org';
+
+export class BrevoMailer {
+	private emailsApi: brevo.TransactionalEmailsApi;
+	private campaignsApi: brevo.EmailCampaignsApi;
+
+	constructor() {
+		this.emailsApi = new brevo.TransactionalEmailsApi();
+		this.emailsApi.setApiKey(0, process.env.BREVO_API_KEY as string);
+
+		this.campaignsApi = new brevo.EmailCampaignsApi();
+		this.campaignsApi.setApiKey(0, process.env.BREVO_API_KEY as string);
+	}
+
+	async sendNewsletterCampaign(
+		subject: string,
+		header: string,
+		content: {
+			title: string;
+			excerpt: string;
+			mainImage: string;
+			footer: string;
+			link: string;
+		}[],
+	) {
+		try {
+			const newsLetterEmailCampaign = new brevo.CreateEmailCampaign();
+
+			newsLetterEmailCampaign.name = subject;
+			newsLetterEmailCampaign.subject = subject;
+			newsLetterEmailCampaign.header = header;
+			newsLetterEmailCampaign.previewText = header;
+			newsLetterEmailCampaign.sender = {
+				name: 'Bryant',
+				email: _BryantEmail,
+			};
+
+			// newsLetterEmailCampaign.toField = _BryantEmail;
+
+			// newsLetterEmailCampaign.templateId = 15;
+			newsLetterEmailCampaign.htmlContent = buildNewsletterTemplate(
+				subject,
+				header,
+				content,
+			);
+
+			newsLetterEmailCampaign.recipients = { listIds: [3] };
+
+			// newsLetterEmailCampaign.params = {
+			// 	parameter: 'My param value',
+			// 	subject: 'common subject',
+			// };
+
+			// schedule send for Tuesday 8AM
+			const tuesday = new Date();
+			tuesday.setDate(tuesday.getDate() + ((2 + 7 - tuesday.getDay()) % 7));
+			tuesday.setHours(8);
+			tuesday.setMinutes(0);
+			tuesday.setSeconds(0);
+			newsLetterEmailCampaign.scheduledAt = tuesday.toISOString();
+
+			const newsletter = await this.campaignsApi.createEmailCampaign(
+				newsLetterEmailCampaign,
+			);
+
+			// send test email to newsletter team
+			this.campaignsApi.sendTestEmail(newsletter.body.id, {
+				emailTo: [_BryantEmail],
+			});
+		} catch (error) {
+			console.error('brevo.createEmailCampaign: ', error.message);
+			throw new Error(error.message);
+		}
+	}
+
+	async sendNewUserEmail(
+		subject: string,
+		header: string,
+		user: UserWithDetails,
+	) {
+		try {
+			return await this.emailsApi.sendTransacEmail({
+				to: [
+					{
+						email: user.email,
+						name: user.firstName,
+					},
+				],
+				sender: {
+					name: 'Bryant',
+					email: 'bryantmejia@grascannabis.org',
+				},
+				templateId: 40,
+				subject,
+				params: { firstName: user.firstName, previewText: header },
+			});
+		} catch (error) {
+			console.error('brevo.sendNewUserEmail: ', error.message);
+			throw new Error(error.message);
+		}
+	}
+
+	async sendNewDispensaryEmail(
+		subject: string,
+		header: string,
+		user: UserWithDetails,
+		organization: Organization,
+	) {
+		try {
+			return await this.emailsApi.sendTransacEmail({
+				to: [
+					{
+						email: user.email,
+						name: user.firstName,
+					},
+				],
+				sender: {
+					name: 'Bryant',
+					email: 'bryantmejia@grascannabis.org',
+				},
+				templateId: 40, // create a different template for dispensaries
+				subject,
+				params: {
+					firstName: user.firstName,
+					previewText: header,
+					dispensary: organization.name,
+				},
+			});
+		} catch (error) {
+			console.error('brevo.sendNewDispensaryEmail: ', error.message);
+			throw new Error(error.message);
+		}
+	}
+
+	async sendNewDeliveryPersonEmail(
+		subject: string,
+		header: string,
+		user: DriverWithDetails,
+	) {
+		try {
+			return await this.emailsApi.sendTransacEmail({
+				to: [
+					{
+						email: user.email,
+						name: user.user.firstName,
+					},
+				],
+				sender: {
+					name: 'Bryant',
+					email: 'bryantmejia@grascannabis.org',
+				},
+				templateId: 40, // create a different template for Delivery persons
+				subject,
+				params: { firstName: user.user.firstName, previewText: header },
+			});
+		} catch (error) {
+			console.error('brevo.sendNewDeliveryPersonEmail: ', error.message);
+			throw new Error(error.message);
+		}
+	}
+}
---
+++ packages/core-lib/lib/sms/sms.module.ts
@@ -0,0 +1,51 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { TextContent, type dispatchEvents } from '../../src';
+import { type SMSAPI } from './Telnyx/telnyx';
+
+class SMSModule {
+	apiName: SMSApiProvider;
+	smsApi: SMSAPI | undefined;
+	constructor(apiName: SMSApiProvider) {
+		switch (apiName) {
+			case 'TextGrid':
+				import('./TextGrid/textgrid').then(
+					(TextGrid) => (this.smsApi = TextGrid.default),
+				);
+				break;
+			case 'Telnyx':
+				import('./Telnyx/telnyx').then(
+					(Telnyx) => (this.smsApi = Telnyx.default),
+				);
+				break;
+			case 'DailyStory':
+				import('./DailyStory/dailystory').then(
+					(DailyStory) => (this.smsApi = DailyStory.default),
+				);
+				break;
+			default:
+				console.info('SMSModule: unhandled apiName ', apiName);
+		}
+		this.apiName = apiName;
+	}
+	async send(
+		event: keyof typeof dispatchEvents,
+		phoneTo: string,
+		data: string,
+	) {
+		try {
+			data += `\n${TextContent.info.CONTACT_SUPPORT}`;
+			data += `\n${TextContent.info.SMS_FOOTER}`;
+			console.info(
+				`sending sms message to ${phoneTo} using ${this.apiName} api: "${data}"`,
+			);
+			const to = `+1${phoneTo}`;
+			this.smsApi?.send(event, to, data);
+		} catch (error) {
+			console.error('SMSModule.send: ', error);
+		}
+	}
+}
+
+export default new SMSModule('DailyStory');
+
+type SMSApiProvider = 'TextGrid' | 'Telnyx' | 'DailyStory';
