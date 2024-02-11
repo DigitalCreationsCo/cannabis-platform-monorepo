220c30393987d1c9396d9bae5de133fa03b8912b
AndresMB <bmejiadeveloper2@gmail.com>
2024-02-10 23:44:53 -0500

    fix(supertokens): updated postgres connection uri, supertokens images, dev and production

--- scripts/supertokens-dev.sh
+++ scripts/supertokens-dev.sh
@@ -1,9 +1,10 @@
 #  Local development image for supertokens
 docker run \
+	--name supertokens \
 	-p 3567:3567 \
-	-e POSTGRESQL_USER=postgres \
+	-e POSTGRESQL_USER=postgres.runhqicnmxfwlzwllswi \
 	-e POSTGRESQL_PASSWORD=RRJV@y_CK-jz9rj \
-	-e POSTGRESQL_HOST=db.runhqicnmxfwlzwllswi.supabase.co \
-	-e POSTGRESQL_PORT=6543 \
+	-e POSTGRESQL_HOST=aws-0-us-west-1.pooler.supabase.com \
+	-e POSTGRESQL_PORT=5432 \
 	-e POSTGRESQL_DATABASE_NAME=postgres \
 	-d registry.supertokens.io/supertokens/supertokens-postgresql:7.0
