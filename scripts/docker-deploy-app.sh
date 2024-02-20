61380cb7a6179d0461ae79a43b37728474be44fd
AndresMB <bmejiadeveloper2@gmail.com>
2024-02-20 05:39:20 -0500

    chore(shop): shop meta-tags

--- scripts/docker-deploy-app.sh
+++ scripts/docker-deploy-app.sh
@@ -5,7 +5,8 @@ PORT=$3
 echo $IMAGE
 echo $TAG
 echo $PORT
+echo 'docker build --cache-from=grasadmin/node_modules:${TAG} -t grasadmin/$IMAGE:$TAG -f Dockerfile.app.local --build-arg=BUILD_TYPE=app --build-arg=BUILD_CONTEXT=$IMAGE --build-arg=PORT=$PORT --no-cache .'
 
-docker build -t grasadmin/$IMAGE:$TAG -f Dockerfile.app --build-arg=BUILD_TYPE=app --build-arg=BUILD_CONTEXT=$IMAGE --build-arg=PORT=$PORT . \
+docker build --cache-from=grasadmin/node_modules:${TAG} -t grasadmin/$IMAGE:$TAG -f Dockerfile.app.local --build-arg=BUILD_TYPE=app --build-arg=BUILD_CONTEXT=$IMAGE --build-arg=PORT=$PORT --no-cache . \
 && docker push grasadmin/$IMAGE:$TAG \
 && kubectl set image deployment/gras-$IMAGE gras-$IMAGE=grasadmin/$IMAGE:$TAG
