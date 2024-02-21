d0bf23494d83a4bc8890e294280d2d81cfbf1a54
AndresMB <bmejiadeveloper2@gmail.com>
2024-02-21 16:44:50 -0500

    chore(shop): add optimonk and gtm script tags WIP

--- app/shop/src/pages/_document.tsx
+++ app/shop/src/pages/_document.tsx
@@ -62,15 +62,9 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 				</Head>
 				<body>
 					{/* <!-- Google Tag Manager (noscript) --> */}
-					<noscript>
-						<iframe
-							title="Google Tag Manager (noscript)"
-							src="https://www.googletagmanager.com/ns.html?id=GTM-WC46C5C2"
-							height="0"
-							width="0"
-							style={{ display: 'none', visibility: 'hidden' }}
-						></iframe>
-					</noscript>
+					<Script
+					dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WC46C5C2"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`}} />
 					{/* <!-- End Google Tag Manager (noscript) --> */}
 					<Main />
 					<NextScript />
