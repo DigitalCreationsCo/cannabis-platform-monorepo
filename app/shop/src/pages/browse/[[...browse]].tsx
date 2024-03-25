6f694fd9b6f09bdbb52a3725dcf5ce611ac3edb7
Bryant's MBP <vibrantceo@Bryantss-MacBook-Pro.local>
2024-03-25 18:23:37 -0400

    chore(shop): map marker image

--- app/shop/src/pages/browse/[[...browse]].tsx
+++ app/shop/src/pages/browse/[[...browse]].tsx
@@ -30,6 +30,7 @@ import { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import useSWR, { type SWRResponse } from 'swr';
 import { twMerge } from 'tailwind-merge';
+import markerImage from '../../../public/marker-30.png';
 import { DispensaryCard, InfoCard } from '../../components';
 // import { shopTour } from '../../tour';
 
@@ -168,6 +169,8 @@ MarketPlace.getLayoutContext = (): LayoutContextProps => ({
 // };
 
 const RenderMapBox = ({ data }: { data: any[] }) => {
+	const [isMarkerSet, setIsMarkerSet] = useState(false);
+
 	mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_API_KEY as string;
 	const mapContainer = useRef(null);
 	const map = useRef<mapboxgl.Map | null>(null);
@@ -190,15 +193,143 @@ const RenderMapBox = ({ data }: { data: any[] }) => {
 			// add markers to map
 			if (!map.current || !geojson) return;
 			for (const feature of geojson.features) {
+				console.info('feature: ', feature);
 				// create a HTML element for each feature
 				const el = document.createElement('div');
 				el.className = 'marker';
-				el.style.backgroundImage = feature.properties.image;
-				el.style.width = `60px`;
-				el.style.height = `60px`;
-				el.textContent = feature.properties.message;
+				el.style.backgroundImage = `url(${feature.properties.image})`;
+				el.style.width = `30px`;
+				el.style.height = `50px`;
 				el.style.backgroundSize = '100%';
 				el.style.cursor = 'pointer';
+
+				// el.className = 'marker';
+				// el.style.backgroundImage = `url(${feature.properties.image})`; // Set background image
+				// el.style.backgroundColor = '#fff'; // Set background color (change #fff to any color you desire)
+				// el.style.display = 'inline-block';
+				// el.style.width = `30px`;
+				// el.style.height = `30px`;
+				// el.style.border = '1px solid #13622a';
+				// el.style.borderWidth = '2px';
+				// el.style.borderRadius = '50%';
+				// // el.textContent = feature.properties.message;
+				// el.style.backgroundSize = 'cover'; // Set background size to cover
+				// el.style.backgroundPosition = 'center'; // Set background position to center
+				// el.style.cursor = 'pointer';
+
+				// Create a container for SVG
+				// const svgContainer = document.createElement('div');
+				// svgContainer.style.cursor = 'pointer';
+				// svgContainer.className = 'marker';
+				// svgContainer.style.width = '50px';
+				// svgContainer.style.height = '50px';
+				// svgContainer.style.position = 'relative';
+
+				// // Create SVG element
+				// const svg = document.createElementNS(
+				// 	'http://www.w3.org/2000/svg',
+				// 	'svg',
+				// );
+				// svg.style.overflow = 'hidden';
+				// svg.setAttribute('viewBox', '4.8878 5.6099 130 130');
+				// svg.setAttribute('width', '62.092');
+				// svg.setAttribute('height', '73.517');
+
+				// // Create ellipse element
+				// const ellipse = document.createElementNS(
+				// 	'http://www.w3.org/2000/svg',
+				// 	'ellipse',
+				// );
+				// ellipse.setAttribute('cx', '93.602');
+				// ellipse.setAttribute('cy', '66.515');
+				// ellipse.setAttribute('rx', '30.351');
+				// ellipse.setAttribute('ry', '30.351');
+				// ellipse.setAttribute(
+				// 	'style',
+				// 	'stroke: #13622a; stroke-width: 6px; fill: #fff;',
+				// );
+
+				// // Create path elements
+				// const path1 = document.createElementNS(
+				// 	'http://www.w3.org/2000/svg',
+				// 	'path',
+				// );
+				// path1.setAttribute(
+				// 	'd',
+				// 	'M 93.228 108.65 C 94.776 110.522 67.77 96.027 62.825 74.35',
+				// );
+				// path1.setAttribute('style', 'stroke: #13622a; fill: #13622a');
+
+				// const path2 = document.createElementNS(
+				// 	'http://www.w3.org/2000/svg',
+				// 	'path',
+				// );
+				// path2.setAttribute(
+				// 	'd',
+				// 	'M 63.209 72.998 C 74.003 110.29 122.582 104.207 124.91 70.755 C 125.06 72.201 123.067 92.899 92.689 109.681',
+				// );
+				// path2.setAttribute('style', 'stroke: #13622a; fill: #13622a');
+
+				// // Create image element
+				// const image = document.createElementNS(
+				// 	'http://www.w3.org/2000/svg',
+				// 	'image',
+				// );
+				// image.setAttributeNS(
+				// 	'http://www.w3.org/1999/xlink',
+				// 	'href',
+				// 	feature.properties.image,
+				// ); // Set image source
+				// image.setAttribute('width', '60'); // Adjust image size as needed
+				// image.setAttribute('height', '60'); // Adjust image size as needed
+				// image.setAttribute('x', '64'); // Adjust x and y position to center the image within the white section of the path
+				// image.setAttribute('y', '36'); // Adjust x and y position to center the image within the white section of the path
+
+				// // Append ellipse and paths to SVG
+				// svg.appendChild(ellipse);
+				// svg.appendChild(path1);
+				// svg.appendChild(path2);
+				// // Append image to SVG
+				// svg.appendChild(image);
+
+				// // // Append SVG to container div
+				// svgContainer.appendChild(svg);
+
+				// 				const svgContainer = document.createElement('div');
+				// 				svgContainer.className = 'marker';
+				// svgContainer.style.width = '50px';
+				// svgContainer.style.height = '50px';
+				// svgContainer.style.position = 'relative';
+
+				// // Create SVG element
+				// const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+				// svg.setAttribute('width', '100%');
+				// svg.setAttribute('height', '100%');
+				// svg.setAttribute('viewBox', '0 0 50 50'); // Adjust viewBox based on marker dimensions
+
+				// // Create path for the marker shape
+				// const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+				// path.setAttribute('d', 'M25 0C34 0 41.3 2.7 46.9 8.1 51.8 13.9 54.8 21.9 54.8 31.2c0 6.5-2.7 12.3-7.1 17.1-3.7 4.2-8.7 7.7-14.2 10.6-5.5-2.9-10.5-6.4-14.2-10.6-4.4-4.8-7.1-10.6-7.1-17.1 0-9.3 3-17.3 8.1-23.1C8.7 2.7 16 0 25 0zM25 7.8c-6.7 0-12.1 5.4-12.1 12.1 0 5.4 7.7 12.9 12.1 17 4.4-4.1 12.1-11.6 12.1-17C37.1 13.2 31.7 7.8 25 7.8z'); // Define the path for the marker shape
+				// path.setAttribute('fill', '#fff'); // Set background color (change #fff to desired color)
+				// path.setAttribute('stroke', '#000'); // Set border color
+				// path.setAttribute('stroke-width', '1px'); // Set border width
+
+				// // Append path to SVG
+				// svg.appendChild(path);
+
+				// // Create image element
+				// const image = document.createElementNS('http://www.w3.org/2000/svg', 'image');
+				// image.setAttributeNS('http://www.w3.org/1999/xlink', 'href', feature.properties.image); // Set image source
+				// image.setAttribute('width', '30'); // Adjust image size as needed
+				// image.setAttribute('height', '30'); // Adjust image size as needed
+				// image.setAttribute('x', '10'); // Adjust x and y position to center the image within the white section of the path
+				// image.setAttribute('y', '10'); // Adjust x and y position to center the image within the white section of the path
+
+				// // Append image to SVG
+				// svg.appendChild(image);
+
+				// // Append SVG to container
+				// svgContainer.appendChild(svg);
 
 				el.addEventListener('click', () => {
 					window.alert(feature.properties.message);
@@ -210,10 +341,11 @@ const RenderMapBox = ({ data }: { data: any[] }) => {
 					.addTo(map.current);
 			}
 		}
-
-		if (data.length > 0 && map.current?.isStyleLoaded)
+		if (data.length > 0 && map.current?.isStyleLoaded && !isMarkerSet) {
 			addMarkersToMapBox(generateGEOJSONDataFromDispensaries(data));
-	}, [data, map.current?.isStyleLoaded]);
+			setIsMarkerSet(true);
+		}
+	}, [data]);
 
 	function generateGEOJSONDataFromDispensaries(
 		data: OrganizationWithShopDetails[],
@@ -233,7 +365,8 @@ const RenderMapBox = ({ data }: { data: any[] }) => {
 					},
 					properties: {
 						message: name,
-						image: dispensary.images[0].location,
+						// image: dispensary.images[0].location || Logo.src,
+						image: markerImage.src,
 					},
 				};
 			}),
