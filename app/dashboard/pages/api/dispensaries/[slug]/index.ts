86004adec2b97678b293b90f7432639dad957745
Bryant Mejia <bmejia220@gmail.com>
2024-06-04 20:55:15 -0400

    saved old shop files, app dashboard daily deals components WIP

--- app/dashboard/pages/api/dispensaries/[slug]/index.ts
+++ app/dashboard/pages/api/dispensaries/[slug]/index.ts
@@ -1,3 +1,11 @@
+import { throwIfNotAllowed, ApiError } from '@cd/core-lib';
+import {
+	deleteDispensary,
+	getDispensary,
+	updateDispensary,
+	type Dispensary,
+} from '@cd/data-access';
+import type { NextApiRequest, NextApiResponse } from 'next';
 import { clientPromise } from '@/lib/db';
 import { throwIfNoDispensaryAccess } from '@/lib/dispensary';
 import env from '@/lib/env';
@@ -5,125 +13,117 @@ import { recordMetric } from '@/lib/metrics';
 import { sendAudit } from '@/lib/retraced';
 import { getCurrentUserWithDispensary } from '@/lib/user';
 import { updateTeamSchema, validateWithSchema } from '@/lib/zod';
-import { throwIfNotAllowed, ApiError } from '@cd/core-lib';
-import {
-  deleteDispensary,
-  getDispensary,
-  updateDispensary,
-  type Dispensary,
-} from '@cd/data-access';
-import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+	req: NextApiRequest,
+	res: NextApiResponse,
 ) {
-  try {
-    await throwIfNoDispensaryAccess(req, res);
+	try {
+		await throwIfNoDispensaryAccess(req, res);
 
-    switch (req.method) {
-      case 'GET':
-        await handleGET(req, res);
-        break;
-      case 'PUT':
-        await handlePUT(req, res);
-        break;
-      case 'DELETE':
-        await handleDELETE(req, res);
-        break;
-      default:
-        res.setHeader('Allow', 'GET, PUT, DELETE');
-        res.status(405).json({
-          error: { message: `Method ${req.method} Not Allowed` },
-        });
-    }
-  } catch (error: any) {
-    const message = error.message || 'Something went wrong';
-    const status = error.status || 500;
+		switch (req.method) {
+			case 'GET':
+				await handleGET(req, res);
+				break;
+			case 'PUT':
+				await handlePUT(req, res);
+				break;
+			case 'DELETE':
+				await handleDELETE(req, res);
+				break;
+			default:
+				res.setHeader('Allow', 'GET, PUT, DELETE');
+				res.status(405).json({
+					error: { message: `Method ${req.method} Not Allowed` },
+				});
+		}
+	} catch (error: any) {
+		const message = error.message || 'Something went wrong';
+		const status = error.status || 500;
 
-    res.status(status).json({ error: { message } });
-  }
+		res.status(status).json({ error: { message } });
+	}
 }
 
 // Get a team by slug
 const handleGET = async (req: NextApiRequest, res: NextApiResponse) => {
-  const user = await getCurrentUserWithDispensary(req, res);
-  const client = await clientPromise;
-  throwIfNotAllowed(user, 'team', 'read');
+	const user = await getCurrentUserWithDispensary(req, res);
+	const client = await clientPromise;
+	throwIfNotAllowed(user, 'team', 'read');
 
-  console.trace('user', user);
-  const team = await getDispensary({ client, where: { id: user.team.id } });
+	console.trace('user', user);
+	const team = await getDispensary({ client, where: { id: user.team.id } });
 
-  recordMetric('team.fetched');
+	recordMetric('team.fetched');
 
-  res.status(200).json({ data: team });
+	res.status(200).json({ data: team });
 };
 
 // Update a team
 const handlePUT = async (req: NextApiRequest, res: NextApiResponse) => {
-  const user = await getCurrentUserWithDispensary(req, res);
-  const client = await clientPromise;
+	const user = await getCurrentUserWithDispensary(req, res);
+	const client = await clientPromise;
 
-  throwIfNotAllowed(user, 'team', 'update');
+	throwIfNotAllowed(user, 'team', 'update');
 
-  const {
-    name,
-    // slug,
-    domain,
-  } = validateWithSchema(updateTeamSchema, req.body);
+	const {
+		name,
+		// slug,
+		domain,
+	} = validateWithSchema(updateTeamSchema, req.body);
 
-  let updatedTeam: Dispensary | null = null;
+	let updatedTeam: Dispensary | null = null;
 
-  try {
-    updatedTeam = await updateDispensary({
-      client,
-      data: { slug: user.team.slug, name, domain: domain as string },
-    });
-  } catch (error: any) {
-    if (error.message.includes('slug')) {
-      throw new ApiError(409, 'This slug is already taken for a team.');
-    }
+	try {
+		updatedTeam = await updateDispensary({
+			client,
+			data: { slug: user.team.slug, name, domain: domain as string },
+		});
+	} catch (error: any) {
+		if (error.message.includes('slug')) {
+			throw new ApiError(409, 'This slug is already taken for a team.');
+		}
 
-    if (error.message.includes('domain')) {
-      throw new ApiError(409, 'This domain is already associated with a team.');
-    }
+		if (error.message.includes('domain')) {
+			throw new ApiError(409, 'This domain is already associated with a team.');
+		}
 
-    throw error;
-  }
+		throw error;
+	}
 
-  sendAudit({
-    action: 'team.update',
-    crud: 'u',
-    user,
-    team: user.team,
-  });
+	sendAudit({
+		action: 'team.update',
+		crud: 'u',
+		user,
+		team: user.team,
+	});
 
-  recordMetric('team.updated');
+	recordMetric('team.updated');
 
-  res.status(200).json({ data: updatedTeam });
+	res.status(200).json({ data: updatedTeam });
 };
 
 // Delete a team
 const handleDELETE = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (!env.teamFeatures.deleteTeam) {
-    throw new ApiError(404, 'Not Found');
-  }
-  const client = await clientPromise;
+	if (!env.teamFeatures.deleteTeam) {
+		throw new ApiError(404, 'Not Found');
+	}
+	const client = await clientPromise;
 
-  const user = await getCurrentUserWithDispensary(req, res);
+	const user = await getCurrentUserWithDispensary(req, res);
 
-  throwIfNotAllowed(user, 'team', 'delete');
+	throwIfNotAllowed(user, 'team', 'delete');
 
-  await deleteDispensary({ client, where: { id: user.team.id } });
+	await deleteDispensary({ client, where: { id: user.team.id } });
 
-  sendAudit({
-    action: 'team.delete',
-    crud: 'd',
-    user,
-    team: user.team,
-  });
+	sendAudit({
+		action: 'team.delete',
+		crud: 'd',
+		user,
+		team: user.team,
+	});
 
-  recordMetric('team.removed');
+	recordMetric('team.removed');
 
-  res.status(204).end();
+	res.status(204).end();
 };
